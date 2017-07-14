00b05dde86ca2f46730664ff6be2c89c21cf65b5
Dan Kass <romeboards@gmail.com>
2017-07-14 12:23:11 -0400

    why no dirname?

--- server/express.js
+++ server/express.js
@@ -16,7 +16,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  // app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build')));
 }
 
 app.use('/', routes);
