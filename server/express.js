3457a459020bfbb0c67c0dbe03e60783d500e4c1
Dan Kass <romeboards@gmail.com>
2017-07-31 12:37:36 -0400

    express handle spa urls

--- server/express.js
+++ server/express.js
@@ -21,4 +21,8 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use('/', routes);
 
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname + 'client/build/index.html'));
+});
+
 module.exports = app;
