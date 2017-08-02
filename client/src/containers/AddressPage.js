71835f55e79918b9d2c76edc79c92cde53db031c
Dan Kass <romeboards@gmail.com>
2017-08-02 00:30:27 -0400

    and property links on the search addr

--- client/src/containers/AddressPage.js
+++ client/src/containers/AddressPage.js
@@ -61,6 +61,8 @@ class AddressPage extends Component {
 
   render() {
 
+    let bbl, boro, block, lot;
+
     if(this.state.hasSearched && this.state.contacts.length === 0)  {
       return (
         <Redirect to={{
@@ -70,12 +72,41 @@ class AddressPage extends Component {
       );
     }
 
+    // I don't like where this is living atm. But fuck it ship it
+    if (this.state.contacts.length) {
+      bbl = this.state.contacts[0].bbl.split('');
+      boro = bbl.slice(0,1).join('');
+      block = bbl.slice(1,6).join('');
+      lot = bbl.slice(6,10).join('');
+    }
+
+
+
+
     return (
       <div className="AddressPage">
         <div className="AddressPage__info">
-          <Link className="btn float-right" to="/">
-            New Search
-          </Link>
+          <div className="btn-group float-right">
+            {this.state.contacts.length ? (
+                <div className="dropdown">
+                  <a href="#" className="btn dropdown-toggle" tabIndex="0">
+                    Property Links &#8964;
+                  </a>
+                  <ul className="menu">
+                    <li><a href={`http://a836-acris.nyc.gov/bblsearch/bblsearch.asp?borough=${boro}&block=${block}&lot=${lot}`} target="_blank">View documents on ACRIS &#8599;</a></li>
+                    <li><a href={`http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?boro=${boro}&block=${block}&lot=${lot}`} target="_blank">DOB Property Profile &#8599;</a></li>
+                    <li><a href={`https://hpdonline.hpdnyc.org/HPDonline/Provide_address.aspx?p1=${boro}&p2=${this.state.searchAddress.housenumber}&p3=${this.state.searchAddress.streetname}&SearchButton=Search`} target="_blank">HPD Complaints/Violations &#8599;</a></li>
+                  </ul>
+                </div>
+              ) : null
+            }
+            <Link className="btn" to="/">
+              New Search
+            </Link>
+          </div>
+
+
+
           <h5 className="primary">Landlord info for {this.state.searchAddress.housenumber} {this.state.searchAddress.streetname}, {this.state.searchAddress.boro}:</h5>
           <OwnersTable
             contacts={this.state.contacts}
