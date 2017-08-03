2c2ec5be94d0c74eb9dac2be01dcc2ac581677ca
Dan Kass <romeboards@gmail.com>
2017-08-03 17:46:30 -0400

    adding keen

--- server/controllers/address.js
+++ server/controllers/address.js
@@ -1,7 +1,14 @@
 const db = require('../services/db'),
       geo = require('../services/geoclient'),
       csv = require('csv-express'),
+      KeenTracking = require('keen-tracking'),
       Promise = require('bluebird');
+
+const client = new KeenTracking({
+  projectId: process.env.KEEN_PROJECT_ID,
+  writeKey: process.env.KEEN_WRITE_KEY
+});
+
 
 const getDataAndFormat = (query) => {
 
@@ -13,12 +20,14 @@ const getDataAndFormat = (query) => {
       // console.dir(geo.address, {depth: null, colors: true});
       if(geo.address.geosupportReturnCode == '00' && geo.address.bbl) {
         console.log('by bbl', geo.address.bbl);
+        query.byBBL = true;
         return Promise.all([
           db.queryContactsByBBL(geo.address.bbl),
           db.queryLandlordsByBBL(geo.address.bbl)
         ]);
       } else {
         console.log('by addr', housenumber, streetname, boro);
+        query.byBBL = false;
         return Promise.all([
           db.queryContactsByAddr(housenumber, streetname, boro),
           db.queryLandlordsByAddr(housenumber, streetname, boro)
@@ -32,7 +41,12 @@ module.exports = {
   query: (req, res) => {
     getDataAndFormat(req.query)
       .then(results => {
-         res.status(200).send({ contacts: results[0], landlords: results[1] });
+        client.recordEvent('search', {
+          query: req.query,
+          contacts: results[0].length ? true : false,
+          landlords: results[1].length
+        });
+        res.status(200).send({ contacts: results[0], landlords: results[1] });
        })
       .catch(err => {
         console.log('err', err);
