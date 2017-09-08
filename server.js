fd0fdb764922604924b7c2a58e42aeb43dcb334d
Dan Kass <romeboards@gmail.com>
2017-09-08 14:44:14 -0400

    setting babel up for prod. needs testing.

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 // Grab .env file if there is one
 require('dotenv').config();
 
+let app;
+
 // This will be our application entry. We'll setup our server here.
-const app = require('./server/express'); // The express app we just created
+if(process.env.NODE_ENV !== 'production') {
+  app = require('./server/express');
+} else {
+  app = require('./dist/express');
+}
 
 const port = parseInt(process.env.PORT, 10) || 3001;
 app.set('port', port);
