5f2fbfad2f72fd754b3e9fb0493b9ab200cc12a3
Dan Kass <romeboards@gmail.com>
2018-05-02 14:42:18 -0400

    tally found addrs 2

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -77,7 +77,7 @@ class HomePage extends Component {
 
       // lets redirect to AddressPage and pass the results along with us
       } else {
-        window.Rollbar.info("Address found!", searchAddress);
+        ga('send', 'event', 'Search', 'search-found');
         return (
           <Redirect to={{
             pathname: `/address/${this.state.searchAddress.boro}/${this.state.searchAddress.housenumber}/${this.state.searchAddress.streetname}`,
