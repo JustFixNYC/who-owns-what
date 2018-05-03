e7053320642b1117ba4654a790da815fb2522b02
Dan Kass <romeboards@gmail.com>
2018-05-03 14:57:42 -0400

    disable service worker

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'containers/App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import 'styles/spectre.css';
 import 'styles/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
