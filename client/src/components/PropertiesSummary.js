0a573fe5b57965adc8fac0b2e36afbc627382fe5
Sam Raby <Sam@Sams-MacBook-Pro-4.local>
2018-10-26 10:01:12 -0400

    changed N/A values to 0 values in rent stab gain/loss summary

--- client/src/components/PropertiesSummary.js
+++ client/src/components/PropertiesSummary.js
@@ -101,7 +101,7 @@ export default class PropertiesSummary extends Component {
               </p>
               <h6>Rent stabilization</h6>
               <p>
-                This portfolio has also an estimated <b>net {agg.totalrsdiff > 0 ? "gain" : "loss"}</b> of <b>{Math.abs(parseInt(agg.totalrsdiff, 10))}</b> rent stabilized units since 2007 (gained {Math.abs(parseInt(agg.totalrsgain, 10))}, lost {Math.abs(parseInt(agg.totalrsloss, 10))}). 
+                This portfolio also had an estimated <b>net {agg.totalrsdiff > 0 ? "gain" : "loss"}</b> of <b>{Math.abs(parseInt(agg.totalrsdiff, 10))}</b> rent stabilized units since 2007 (gained {Math.abs(parseInt(agg.totalrsgain, 10))}, lost {Math.abs(parseInt(agg.totalrsloss, 10))}). 
                 This represents <b>{agg.rsproportion}%</b> of the total size of this portfolio. The building that has lost the most units is&nbsp;
                 {agg.rslossaddr && (
                   <b>
