ba64d6ed08bb56d6a63afcc7e5916d551eb46ca3
Atul Varma <varmaa@gmail.com>
2018-11-05 16:53:28 -0500

    Add builddb.py.

---
+++ builddb.py
@@ -0,0 +1,61 @@
+import psycopg2
+import os
+import subprocess
+from pathlib import Path
+from types import SimpleNamespace
+
+IS_TESTING = True
+
+ROOT_DIR = Path(__file__).parent.resolve()
+
+DB_HOST = os.environ.get('DB_HOST', 'db')
+DB_DATABASE = os.environ.get('DB_DATABASE', 'wow')
+DB_USER = os.environ.get('DB_USER', 'wow')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'wow')
+DB_PORT = os.environ.get('DB_PORT', '5432')
+
+if IS_TESTING:
+    DATA_DIR = ROOT_DIR / 'tests' / 'data'
+else:
+    DATA_DIR = ROOT_DIR / 'nycdb' / 'data'
+
+
+DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def call_nycdb(*args):
+    subprocess.check_call(
+        ['nycdb', *args, '-H', DB_HOST, '-U', DB_USER, '-P', DB_PASSWORD,
+         '-D', DB_DATABASE, '--port', DB_PORT, '--root-dir', str(DATA_DIR)]
+    )
+
+
+def connection():
+    return psycopg2.connect(
+        user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database=DB_DATABASE, port=DB_PORT)
+
+
+def does_table_exist(conn, name):
+    with conn:
+        with conn.cursor() as cursor:
+            cursor.execute(f"SELECT to_regclass('public.{name}')")
+            return cursor.fetchone()[0] is not None
+
+
+def ensure_table(conn, name):
+    if not does_table_exist(conn, name):
+        print(f"Table {name} not found in the database. Downloading...")
+        call_nycdb('--download', name)
+        print(f"Loading {name} into the database...")
+        call_nycdb('--load', name)
+    elif not IS_TESTING:
+        print(f"Table {name} already exists. Verifying row count...")
+        call_nycdb('--verify', name)
+
+
+if __name__ == '__main__':
+    conn = connection()
+    ensure_table(conn, 'pluto_18v1')
+    ensure_table(conn, 'rentstab_summary')
+    ensure_table(conn, 'marshal_evictions_17')
+    ensure_table(conn, 'hpd_registrations')
