fda2d88fd5db312e7b4d7298d310e0692f4694eb
Sam Raby <Sam@Sams-MBP-4.brl.nyc>
2019-02-28 15:35:52 -0500

    added chartjs

--- client/src/components/Indicators.js
+++ client/src/components/Indicators.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import RC2 from 'react-chartjs2';
 
 import Loader from 'components/Loader';
 import LegalFooter from 'components/LegalFooter';
@@ -36,6 +37,43 @@ export default class Indicators extends Component {
   }
 
   render() {
+
+    var data = {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    };
+
+    var options = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    };
+
+
     return (
       <div className="Page PropertiesSummary">
         <div className="PropertiesSummary__content Page__content">
@@ -44,6 +82,7 @@ export default class Indicators extends Component {
             ) : 
           (
             <div>
+              <RC2 data={data} options={options} type='bar' />
               <h3>Recent Sales:</h3> 
               <ul>
                 {this.state.saleHistory && 
