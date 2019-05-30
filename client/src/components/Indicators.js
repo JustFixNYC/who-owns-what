489c8fb67178c5cfd2d0afd0ef24c31544ee65df
Sam Raby <Sam@Sams-MBP-4.brl.nyc>
2019-05-30 14:08:38 -0400

    reworked infobox text

--- client/src/components/Indicators.js
+++ client/src/components/Indicators.js
@@ -400,7 +400,12 @@ export default class Indicators extends Component {
                   <div className="card-body">
                     {(this.state.activeVis === 'complaints' ? 
                       <span>HPD Complaints are housing issues reported to the City <b>by a tenant calling 311</b>.
-                      When someone issues a complaint, the Department of Housing Preservation and Development begins a process of investigation that may lead to an official violation from the City. 
+                      When someone issues a complaint, the Department of Housing Preservation and Development begins a process of investigation that may lead to an official violation from the City.
+                      Complaints can be identified as:<br/>
+                        <br/>
+                      <b>Emergency</b> — reported to be hazardous/dire<br/>
+                      <b>Non-Emergency</b> — all others<br/>
+                        <br/>  
                       Read more about HPD Complaints and how to file them at the <a href='https://www1.nyc.gov/site/hpd/renters/complaints-and-inspections.page' target="_blank" rel="noopener noreferrer">official HPD page</a>.</span> : 
                     this.state.activeVis === 'viols' ? 
                       <span>HPD Violations occur when an official City Inspector finds the conditions of a home in violation of the law. 
@@ -414,7 +419,9 @@ export default class Indicators extends Component {
                       Read more about HPD Violations at the <a href='https://www1.nyc.gov/site/hpd/owners/compliance-maintenance-requirements.page' target="_blank" rel="noopener noreferrer">official HPD page</a>.</span> :
                     this.state.activeVis === 'permits' ? 
                       <span>Owners submit Building Permit Applications to the Department of Buildings before any construction project to get necessary approval.
-                      The number of applications filed can indicate how much construction the owner was planning. 
+                      The number of applications filed can indicate how much construction the owner was planning.
+                        <br/>
+                        <br/> 
                       Read more about DOB Building Applications/Permits at the <a href='https://www1.nyc.gov/site/buildings/about/building-applications-and-permits.page' target="_blank" rel="noopener noreferrer">official NYC Buildings page</a>.</span> :
                     '')}
                   </div>
