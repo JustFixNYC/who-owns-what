7e84c7de4299d23051b5b8f7d16ed36e99e95d0a
Sam Raby <Sam@Sams-MBP-4.brl.nyc>
2019-06-25 14:56:44 -0400

    added fullstory via react-fullstory module
    and removed site tag on index.html

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -6,6 +6,7 @@ import {
   NavLink,
   Link
 } from 'react-router-dom';
+import FullStory from 'react-fullstory';
 
 import 'styles/App.css';
 
@@ -36,45 +37,46 @@ render() {
     return (
       <Router>
         <ScrollToTop>
-        <div className="App">
-          <div className="App__warning old_safari_only">
-            <h3>Warning! This site doesn't fully work on older versions of Safari. Try a <a href="http://outdatedbrowser.com/en">modern browser</a>.</h3>
+          <div className="App">
+            <FullStory org="MBQ2E" />
+            <div className="App__warning old_safari_only">
+              <h3>Warning! This site doesn't fully work on older versions of Safari. Try a <a href="http://outdatedbrowser.com/en">modern browser</a>.</h3>
+            </div>
+            <div className="App__header">
+              <Link onClick={() => {window.gtag('event', 'site-title');}} to="/">
+                <h4>Who owns what in nyc?</h4>
+              </Link>
+              <nav className="inline">
+                <NavLink exact to="/">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/how-it-works">How it Works</NavLink>
+                <a href="https://www.justfix.nyc/donate">Donate</a>
+                <a href="#" // eslint-disable-line jsx-a11y/anchor-is-valid
+                  onClick={() => this.setState({ showEngageModal: true })}>
+                  Share
+                </a>
+              </nav>
+              <Modal
+                showModal={this.state.showEngageModal}
+                onClose={() => this.setState({ showEngageModal: false })}>
+                  <h5 className="first-header">Share this page with your neighbors:</h5>
+                  <SocialShare location="share-modal" />
+              </Modal>
+            </div>
+            <div className="App__body">
+              <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/not-found" component={NotRegisteredPage} />
+                <Route path="/address/:boro/:housenumber/:streetname" component={AddressPage} />
+                <Route path="/bbl/:boro/:block/:lot" component={BBLPage} />
+                <Route path="/bbl/:bbl" component={BBLPage} />
+                <Route path="/about" component={AboutPage} />
+                <Route path="/how-it-works" component={HowItWorksPage} />
+                <Route path="/terms-of-use" component={TermsOfUsePage} />
+                <Route path="/privacy-policy" component={PrivacyPolicyPage} />
+              </Switch>
+            </div>
           </div>
-          <div className="App__header">
-            <Link onClick={() => {window.gtag('event', 'site-title');}} to="/">
-              <h4>Who owns what in nyc?</h4>
-            </Link>
-            <nav className="inline">
-              <NavLink exact to="/">Home</NavLink>
-              <NavLink to="/about">About</NavLink>
-              <NavLink to="/how-it-works">How it Works</NavLink>
-              <a href="https://www.justfix.nyc/donate">Donate</a>
-              <a href="#" // eslint-disable-line jsx-a11y/anchor-is-valid
-                 onClick={() => this.setState({ showEngageModal: true })}>
-                Share
-              </a>
-            </nav>
-            <Modal
-              showModal={this.state.showEngageModal}
-              onClose={() => this.setState({ showEngageModal: false })}>
-                <h5 className="first-header">Share this page with your neighbors:</h5>
-                <SocialShare location="share-modal" />
-            </Modal>
-          </div>
-          <div className="App__body">
-            <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route path="/not-found" component={NotRegisteredPage} />
-              <Route path="/address/:boro/:housenumber/:streetname" component={AddressPage} />
-              <Route path="/bbl/:boro/:block/:lot" component={BBLPage} />
-              <Route path="/bbl/:bbl" component={BBLPage} />
-              <Route path="/about" component={AboutPage} />
-              <Route path="/how-it-works" component={HowItWorksPage} />
-              <Route path="/terms-of-use" component={TermsOfUsePage} />
-              <Route path="/privacy-policy" component={PrivacyPolicyPage} />
-            </Switch>
-          </div>
-        </div>
         </ScrollToTop>
       </Router>
     );
