50bc97a7c1e71f97c3252f82c0d3d082eefc1e7f
Sam Raby <Sam@Sams-MBP-4.fios-router.home>
2019-08-08 17:06:01 -0400

    restructured not registered page to match nycha page

--- client/src/containers/NychaPage.js
+++ client/src/containers/NychaPage.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import LegalFooter from 'components/LegalFooter';
 import Helpers from 'util/helpers';
 import APIClient from 'components/APIClient';
+import SocialShare from 'components/SocialShare';
 
 import 'styles/NotRegisteredPage.css';
 
@@ -28,7 +29,7 @@ export default class NychaPage extends Component {
 
   render() {
     const geosearch = this.props.geosearch;
-    // const searchAddress = this.props.searchAddress;
+    const searchAddress = this.props.searchAddress;
     const buildingInfo = (this.state.buildingInfo && this.state.buildingInfo.length > 0 ? this.state.buildingInfo[0] : null);
 
     let boro, block, lot;
@@ -66,6 +67,21 @@ export default class NychaPage extends Component {
           boroOfficePhone: '(718) 553-4700'
         } : null );
 
+    const usersInputAddress = 
+      (searchAddress && searchAddress.boro && (searchAddress.housenumber || searchAddress.streetname) ?
+        { 
+          boro: searchAddress.boro,
+          housenumber: (searchAddress.housenumber || ' '),
+          streetname: (searchAddress.streetname || ' ') 
+        } :
+        buildingInfo && buildingInfo.boro && (buildingInfo.housenumber || buildingInfo.streetname) ?
+        { 
+          boro: buildingInfo.boro,
+          housenumber: (buildingInfo.housenumber || ' '),
+          streetname: (buildingInfo.streetname || ' ') 
+        } : 
+        null );
+
     if(!geosearch && !buildingInfo) {
       return (
         <div className="NotRegisteredPage Page">
@@ -90,101 +106,96 @@ export default class NychaPage extends Component {
             <h6 className="mt-10 text-center text-bold text-large">
               This building is owned by the NYC Housing Authority (NYCHA)
             </h6>
-            <div className="card-body">
-              <div className="card-body-table">
-                <div className="table-row">
-                  <div title="The city borough where your search address is located">
-                    <label>Borough</label>
-                    { boroData && boroData.boroName }
-                  </div>
-                  <div title="The number of residential units across all buildings in this development, according to the Dept. of City Planning.">
-                    <label>Units</label>
-                    { nycha.dev_unitsres }
-                  </div>
-                  <div title="Evictions executed in this development by NYC Marshals in 2018. City Council, the Housing Data Coalition and Anti-Eviction Mapping Project cleaned, geocoded, and validated the data, originally sourced from DOI.">
-                    <label>2018 Evictions</label>
-                    { nycha.dev_evictions }
-                  </div>
-                </div>
-              </div>
-              <div className="columns nycha-addresses">
-              { boroData && 
-                <div className="column col-lg-12 col-6">
-                  <div title="The NYCHA office overseeing your borough. Some experts suggest reaching out to Borough Management Offices to advocate for repairs, as they tend to have more administrative power than local management offices.">
-                    <b>Borough Management Office:</b>
-                  </div>
-                    <ul>
-                      <li>{boroData.boroOfficeAddress1}, {boroData.boroOfficeAddress2}</li>
-                      <li>{boroData.boroOfficePhone}</li>
-                    </ul>
-              
-                </div> }
-                <div className="column col-lg-12 col-6">
-                  <div title="The federal HUD office overseeing New York State. Some experts suggest reaching out to the Regional Office to advocate for repairs, as they tend to have more administrative power than local management offices." >
-                    <b>New York Regional Office:</b>
-                  </div>
-                  <ul>
-                    <li>26 Federal Plaza, New York, NY 10278</li>
-                    <li>(212) 264-1290</li>
-                    <li>complaints_office_02@hud.gov</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            
-        
-
-                    {/* { boroData && 
-                    (<div className="table-row">
-                      <div title="Contact info for the NYCHA office overseeing your borough. Some experts suggest reaching out to Borough Management Offices to advocate for repairs, as they tend to have more administrative power than local management offices.">
-                        <label>Borough Management Office</label>
-                        {boroData.boroOfficeAddress1}<span className="hide-lg">, </span><br className="show-lg" />{boroData.boroOfficeAddress2}<br/>
-                        {boroData.boroOfficePhone}
-                      </div>
-                    </div>) }
+            <div className="wrapper">
+              <div className="card-body">
+                <div className="card-body-table">
                   <div className="table-row">
-                    <div title="Contact info for the federal office overseeing all of New York State and New Jersey. Some experts suggest reaching out to the Regional Office to advocate for repairs, as they tend to have more administrative power than local management offices.">
-                      <label>New York Regional Office</label>
-                      26 Federal Plaza<span className="hide-lg">, </span><br className="show-lg" />New York, NY 10278<br/>
-                      (212) 264-1290<br/>
-                      complaints_office_02@hud.gov
+                    <div title="The city borough where your search address is located">
+                      <label>Borough</label>
+                      { boroData && boroData.boroName }
                     </div>
-                  </div> */}
-     
-
-              { buildingInfo && buildingInfo.latitude && buildingInfo.longitude &&
-            <img src={`https://maps.googleapis.com/maps/api/streetview?size=800x200&location=${buildingInfo.latitude},${buildingInfo.longitude}&key=${process.env.REACT_APP_STREETVIEW_API_KEY}`}
-                 alt="Google Street View" className="img-responsive"  />
-              }
-            <div className="bbl-link">
-              { geosearch && geosearch.bbl && buildingInfo ? (<span>Boro-Block-Lot (BBL): <nobr><a href={"https://zola.planning.nyc.gov/lot/"+boro + "/" + block + "/" + lot} target="_blank" rel="noopener noreferrer">{boro}{bblDash}{block}{bblDash}{lot}</a></nobr></span>):(<span></span>) }
-            </div>
-            <br />
-            
-              <div>
-                <p>Useful links:</p>
-                <div>
-                  <div className="btn-group btn-group-block">
-                    <a href="https://www.hud.gov/sites/documents/958.PDF" target="_blank" rel="noopener noreferrer" className="btn">HUD Complaint Form 958 &#8599;</a>
-                    <a href={`http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?boro=${boro}&block=${block}&lot=${lot}`} target="_blank" rel="noopener noreferrer" className="btn">DOB Building Profile &#8599;</a>
+                    <div title="The number of residential units across all buildings in this development, according to the Dept. of City Planning.">
+                      <label>Units</label>
+                      { nycha.dev_unitsres }
+                    </div>
+                    <div title="Evictions executed in this development by NYC Marshals in 2018. City Council, the Housing Data Coalition and Anti-Eviction Mapping Project cleaned, geocoded, and validated the data, originally sourced from DOI.">
+                      <label>2018 Evictions</label>
+                      { nycha.dev_evictions }
+                    </div>
                   </div>
-                  <div className="btn-group btn-group-block">
-                    <a href="https://www1.nyc.gov/assets/nycha/downloads/pdf/Development-Guide-01142019.pdf" target="_blank" rel="noopener noreferrer" className="btn">NYCHA Facility Directory &#8599;</a>
-                    <a href="https://www1.nyc.gov/site/nycha/mynycha/mynycha-landing.page" target="_blank" rel="noopener noreferrer" className="btn">MyNYCHA App &#8599;</a>
+                </div>
+                <div className="columns nycha-addresses">
+                { boroData && 
+                  <div className="column col-lg-12 col-6">
+                    <div title="The NYCHA office overseeing your borough. Some experts suggest reaching out to Borough Management Offices to advocate for repairs, as they tend to have more administrative power than local management offices.">
+                      <b>Borough Management Office:</b>
+                    </div>
+                      <ul>
+                        <li>{boroData.boroOfficeAddress1}, {boroData.boroOfficeAddress2}</li>
+                        <li>{boroData.boroOfficePhone}</li>
+                      </ul>
+                
+                  </div> }
+                  <div className="column col-lg-12 col-6">
+                    <div title="The federal HUD office overseeing New York State. Some experts suggest reaching out to the Regional Office to advocate for repairs, as they tend to have more administrative power than local management offices." >
+                      <b>New York Regional Office:</b>
+                    </div>
+                    <ul>
+                      <li>26 Federal Plaza, New York, NY 10278</li>
+                      <li>(212) 264-1290</li>
+                      <li>complaints_office_02@hud.gov</li>
+                    </ul>
                   </div>
                 </div>
               </div>
             
+                { buildingInfo && buildingInfo.latitude && buildingInfo.longitude &&
+              <img src={`https://maps.googleapis.com/maps/api/streetview?size=800x200&location=${buildingInfo.latitude},${buildingInfo.longitude}&key=${process.env.REACT_APP_STREETVIEW_API_KEY}`}
+                  alt="Google Street View" className="streetview img-responsive"  />
+                }
+              <div className="bbl-link">
+                { geosearch && geosearch.bbl && buildingInfo ? (<span>Boro-Block-Lot (BBL): <nobr><a href={"https://zola.planning.nyc.gov/lot/"+boro + "/" + block + "/" + lot} target="_blank" rel="noopener noreferrer">{boro}{bblDash}{block}{bblDash}{lot}</a></nobr></span>):(<span></span>) }
+              </div>
+              <br />
+              
+                <div>
+                  <p>Useful links:</p>
+                  <div>
+                    <div className="btn-group btn-group-block">
+                      <a href="https://www.hud.gov/sites/documents/958.PDF" target="_blank" rel="noopener noreferrer" className="btn">HUD Complaint Form 958 &#8599;</a>
+                      <a href={`http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?boro=${boro}&block=${block}&lot=${lot}`} target="_blank" rel="noopener noreferrer" className="btn">DOB Building Profile &#8599;</a>
+                    </div>
+                    <div className="btn-group btn-group-block">
+                      <a href="https://www1.nyc.gov/assets/nycha/downloads/pdf/Development-Guide-01142019.pdf" target="_blank" rel="noopener noreferrer" className="btn">NYCHA Facility Directory &#8599;</a>
+                      <a href="https://www1.nyc.gov/site/nycha/mynycha/mynycha-landing.page" target="_blank" rel="noopener noreferrer" className="btn">MyNYCHA App &#8599;</a>
+                    </div>
+                  </div>
+                </div>
 
-            <br />
-            {/* <div className="toast toast-error">
-              <u>Note:</u> We're currently experiencing some difficulties due to an official NYC data service failing. We're working on it. If a search returns with "no results found", try it again in a minute or so!
-            </div> */}
-            <br />
+                <div className="justfix-cta">
+                  <p>Are you having issues in this development?</p>
+                  <a onClick={() => {window.gtag('event', 'take-action-nycha-page');}} href={`https://app.justfix.nyc?utm_source=whoownswhat`} target="_blank" rel="noopener noreferrer" className="btn btn-justfix btn-block">Take action on JustFix.nyc!</a>
+                </div>
 
-            <Link className="btn btn-primary btn-block" to="/">
-              &lt;-- Search for a different address
-            </Link>
+                <div className="social-share">
+                  <p>Share this page with your neighbors:</p>
+                  <SocialShare 
+                    location="nycha-page"
+                    url={usersInputAddress && encodeURI('https://whoownswhat.justfix.nyc/address/' + usersInputAddress.boro + '/' + usersInputAddress.housenumber + '/' + usersInputAddress.streetname).replace(" ", "%20")} // Support for Android
+                    />
+                </div>
+    
+
+              <br />
+              {/* <div className="toast toast-error">
+                <u>Note:</u> We're currently experiencing some difficulties due to an official NYC data service failing. We're working on it. If a search returns with "no results found", try it again in a minute or so!
+              </div> */}
+              <br />
+
+              <Link className="btn btn-primary btn-block" to="/">
+                &lt;-- Search for a different address
+              </Link>
+            </div>
           </div>
         </div>
         <LegalFooter />
