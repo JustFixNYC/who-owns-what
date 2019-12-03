39b15e9afebab1a73443cdc29f04ab91101248ad
Atul Varma <varmaa@gmail.com>
2019-12-03 16:07:19 -0500

    Set app element on Modal.

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,18 @@ import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 import App from 'containers/App';
 
 // import registerServiceWorker from './registerServiceWorker';
 import 'styles/spectre.css';
 import 'styles/index.css';
 
+const root = document.getElementById('root');
+Modal.setAppElement(root);
+
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  root
 );
 // registerServiceWorker();
