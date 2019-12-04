1fc6b570a659e780ae6d98c255629104a64224b5
Atul Varma <varmaa@gmail.com>
2019-12-04 08:51:28 -0500

    Improve/simplify logic a bit, maybe.

--- client/src/components/StreetView.tsx
+++ client/src/components/StreetView.tsx
@@ -3,7 +3,6 @@ import { withGoogleMap, StreetViewPanorama } from 'react-google-maps';
 import Browser from '../util/browser';
 
 export type StreetViewAddr = {
-  bbl: string,
   lat: number,
   lng: number
 };
@@ -12,14 +11,14 @@ export type StreetViewProps = {
   addr: StreetViewAddr|null|undefined,
 };
 
-type ImplProps = {
+type HeadingAndCoords = {
   heading: number,
   coordinates: google.maps.LatLng|undefined
 };
 
-type State = ImplProps;
+type State = HeadingAndCoords;
 
-const UnwrappedStreetViewImpl: React.FC<ImplProps> = props => {
+const UnwrappedStreetViewImpl: React.FC<HeadingAndCoords> = props => {
   return (
     <StreetViewPanorama
       visible
@@ -39,15 +38,20 @@ const UnwrappedStreetViewImpl: React.FC<ImplProps> = props => {
 
 const StreetViewImpl = withGoogleMap(UnwrappedStreetViewImpl);
 
+const NULL_HEADING_AND_COORDS: HeadingAndCoords = { heading: 0, coordinates: undefined };
+
+function getAddrHash(addr: StreetViewAddr|null|undefined): string {
+  return addr ? `${addr.lat},${addr.lng}` : '';
+}
+
 export class StreetView extends React.Component<StreetViewProps, State> {
   _isMounted: boolean = false;
+  _currAddrHash: string;
 
   constructor(props: StreetViewProps) {
     super(props);
-    this.state = {
-      heading: 0,
-      coordinates: undefined
-    };
+    this._currAddrHash = getAddrHash(null);
+    this.state = {...NULL_HEADING_AND_COORDS};
   }
 
   componentDidMount() {
@@ -60,39 +64,39 @@ export class StreetView extends React.Component<StreetViewProps, State> {
   }
 
   updateHeadingAndCoordinates() {
-    if (!this.props.addr) {
-      this.setState({
-        heading: 0,
-        coordinates: undefined  
-      });
+    const addrHash = getAddrHash(this.props.addr);
+    if (addrHash === this._currAddrHash) {
+      // Nothing changed, no need to recompute anything.
       return;
     }
 
-    let coordinates = new window.google.maps.LatLng(this.props.addr.lat, this.props.addr.lng);
-    let streetViewService = new window.google.maps.StreetViewService();
+    this._currAddrHash = addrHash;
 
-    streetViewService.getPanoramaByLocation(coordinates, 50, (panoData) => {
-      if (!this._isMounted) return;
-      if (panoData && panoData.location && panoData.location.latLng) {
-        // this computes the street view heading
-        // srsly tho google, why not point to the latlng automatically?
-        let panoCoordinates = panoData.location.latLng;
-        this.setState({
-          heading: window.google.maps.geometry.spherical.computeHeading(panoCoordinates, coordinates),
-          coordinates: coordinates
-        });
-      }
-    });
+    if (this.props.addr) {
+      let coordinates = new window.google.maps.LatLng(this.props.addr.lat, this.props.addr.lng);
+      let streetViewService = new window.google.maps.StreetViewService();
+
+      streetViewService.getPanoramaByLocation(coordinates, 50, (panoData) => {
+        if (!(this._isMounted && addrHash === this._currAddrHash)) return;
+        if (panoData && panoData.location && panoData.location.latLng) {
+          // this computes the street view heading
+          // srsly tho google, why not point to the latlng automatically?
+          let panoCoordinates = panoData.location.latLng;
+          this.setState({
+            heading: window.google.maps.geometry.spherical.computeHeading(panoCoordinates, coordinates),
+            coordinates: coordinates
+          });
+        } else {
+          this.setState(NULL_HEADING_AND_COORDS);
+        }
+      });
+    } else {
+      this.setState(NULL_HEADING_AND_COORDS);
+    }
   }
 
-  // we need to trigger an ajax call (the streetViewService) when props
-  // receives a new address.
-  componentDidUpdate(prevProps: StreetViewProps) {
-    // this says: if the component is getting the addr for the first time OR
-    //            if the component already has an addr but is getting a new one
-    if( (!prevProps.addr && this.props.addr) || (prevProps.addr && this.props.addr && (prevProps.addr.bbl !== this.props.addr.bbl))) {
-      this.updateHeadingAndCoordinates();
-    }
+  componentDidUpdate() {
+    this.updateHeadingAndCoordinates();
   }
 
   render() {
