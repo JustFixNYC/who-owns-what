dc0fc96dcb0909ba4a96ca75658de8cd2fde4da8
Atul Varma <varmaa@gmail.com>
2019-12-09 06:30:00 -0500

    Convert mapping.js to TS and add tests.

---
+++ client/src/util/mapping.test.ts
@@ -0,0 +1,16 @@
+import mapping from './mapping';
+
+test("latLngIsNull() works", () => {
+  expect(mapping.latLngIsNull([NaN, 1])).toBe(true);
+  expect(mapping.latLngIsNull([1, NaN])).toBe(true);
+  expect(mapping.latLngIsNull([NaN, NaN])).toBe(true);
+  expect(mapping.latLngIsNull([1, 2])).toBe(false);
+});
+
+test("getBoundingBox() works", () => {
+  expect(mapping.getBoundingBox([
+    [2, 3],
+    [0.5, 2.2],
+    [0, 1],
+  ])).toEqual([[0, 1], [2, 3]]);
+});
