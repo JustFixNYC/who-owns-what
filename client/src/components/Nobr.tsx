1309250a3ac3cd8f680f35eb8ff1a2d98a8d4b54
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2019-12-09 12:17:16 -0500

    Hotfix: Define React inside Nobr component

--- client/src/components/Nobr.tsx
+++ client/src/components/Nobr.tsx
@@ -1,4 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/nobr
+import React from 'react';
 
 export const Nobr: React.FC<{}> = (props) => 
   <span style={{whiteSpace: 'nowrap'}}>{props.children}</span>;
