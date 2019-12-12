f99c6beda300f37560b1531810b0251a2fe79ccf
Atul Varma <varmaa@gmail.com>
2019-12-12 16:19:37 -0500

    Add 'yarn typecheck' and run it during CI.

--- client/src/components/AddressSearch.test.tsx
+++ client/src/components/AddressSearch.test.tsx
@@ -3,6 +3,7 @@ import { searchAddressToString } from "./AddressSearch";
 describe("searchAddressToString()", () => {
   it("works when housenumber is a string", () => {
     expect(searchAddressToString({
+      bbl: '1234567890',
       housenumber: '150',
       streetname: 'COURT STREET',
       boro: 'BROOKLYN'
@@ -11,6 +12,7 @@ describe("searchAddressToString()", () => {
 
   it("works when housenumber is undefined", () => {
     expect(searchAddressToString({
+      bbl: '1234567890',
       streetname: 'GOWANUS HOUSES',
       boro: 'BROOKLYN'
     })).toBe('GOWANUS HOUSES, BROOKLYN');
