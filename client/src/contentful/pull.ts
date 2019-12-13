9389cd03c3f95d41e4248ad7bcdc887d8ea6dd56
Atul Varma <varmaa@gmail.com>
2019-12-13 07:32:23 -0500

    Only write files if content has changed.

--- client/src/contentful/pull.ts
+++ client/src/contentful/pull.ts
@@ -8,6 +8,17 @@ const CONTENTFUL_SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
 const CONTENTFUL_ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
 const DATA_DIR = path.join(__dirname, '..', 'data');
 
+function writeFileIfChangedSync(absPath: string, content: string, encoding = 'utf-8'): boolean {
+  if (fs.existsSync(absPath)) {
+    const currContents = fs.readFileSync(absPath, {encoding});
+    if (currContents === content) {
+      return false;
+    }
+  }
+  fs.writeFileSync(absPath, content, {encoding});
+  return true;
+}
+
 /**
  * Pull all content types from Contentful and serialize them to JSON.
  */
@@ -24,7 +35,11 @@ export async function pullFromContentful() {
     accessToken: CONTENTFUL_ACCESS_TOKEN,
   });
 
-  for (let locale of getSupportedLocales()) {
+  let numProcessed = 0;
+  let numChanged = 0;
+  const locales = getSupportedLocales();
+
+  for (let locale of locales) {
     const entries = await client.getEntries<PageFields>({
       'content_type': PageContentType,
       'locale': locale,
@@ -33,10 +48,17 @@ export async function pullFromContentful() {
     entries.items.forEach(entry => {
       const { locale } = entry.sys;
       const filename = `${entry.fields.slug}.${locale}.json`;
-      console.log(`Exporting ${filename}.`);
-      fs.writeFileSync(path.join(DATA_DIR, filename), JSON.stringify(entry.fields, null, 2), {
-        encoding: 'utf-8',
-      });
+      const content = JSON.stringify(entry.fields, null, 2);
+      numProcessed += 1;
+      if (writeFileIfChangedSync(path.join(DATA_DIR, filename), content)) {
+        console.log(`Exported ${filename}.`);
+        numChanged += 1;
+      }
     });
+  }
+
+  console.log(`Finished processing ${numProcessed} localized pages.`);
+  if (numChanged === 0) {
+    console.log(`Nothing changed (no files were written).`);
   }
 }
