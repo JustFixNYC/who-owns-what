364e4c503d4c9633a0f0a5c3d99101801b6915f9
Atul Varma <varmaa@gmail.com>
2020-01-16 15:17:50 -0500

    Oops, fix bugs in eviction summary i18n.

--- client/src/components/EvictionsSummary.tsx
+++ client/src/components/EvictionsSummary.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import helpers, { MaybeStringyNumber } from '../util/helpers';
-import { Plural } from '@lingui/react';
-
-const Evictions: React.FC<{count: number}> = ({ count }) => <Plural
-  value={count}
-  one="one eviction"
-  other="# evictions"
-/>;
+import { Trans, Plural } from '@lingui/macro';
 
 export const EvictionsSummary: React.FC<{
   totalevictions: MaybeStringyNumber,
@@ -19,13 +13,13 @@ export const EvictionsSummary: React.FC<{
 }> = props => {
   const totalEvictions = helpers.coerceToInt(props.totalevictions, 0);
   const building = props.evictionsaddr;
+  const buildingTotalEvictions = helpers.coerceToInt(building && building.evictions, 0);
 
   return <p>
-    In 2018, NYC Marshals scheduled <Evictions count={totalEvictions} /> across this portfolio.{" "}
-    {building && <>
-      The building with the most evictions was&nbsp;
-      <b>{building.housenumber} {building.streetname}, {building.boro}</b>{" "}
-      with <Evictions count={totalEvictions} /> that year.
-    </>}
+    <Trans>In 2018, NYC Marshals scheduled <Plural value={totalEvictions} one="one eviction" other="# evictions" /> across this portfolio.</Trans>
+    {" "}
+    {building && <Trans>
+      The building with the most evictions was <b>{building.housenumber} {building.streetname}, {building.boro}</b> with <Plural value={buildingTotalEvictions} one="one eviction" other="# evictions" /> that year.
+    </Trans>}
   </p>;
 }
