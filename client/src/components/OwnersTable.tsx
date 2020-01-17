0e80b0a1409ef3fcdff7a39f52addebbc634d9de
Atul Varma <varmaa@gmail.com>
2020-01-17 12:10:32 -0500

    Internationalized owners table.

--- client/src/components/OwnersTable.tsx
+++ client/src/components/OwnersTable.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'styles/OwnersTable.css';
 
 import _isEmpty from 'lodash/isEmpty';
+import { Trans } from '@lingui/macro';
 
 const OwnersTable: React.FC<{
   addr: {
@@ -25,7 +26,7 @@ const OwnersTable: React.FC<{
         <div className="container">
           <div className="columns">
             <div className="column col-3">
-              <p>Business Addresses</p>
+              <Trans render="p">Business Addresses</Trans>
               { props.addr.businessaddrs && props.addr.businessaddrs.length && (
                 <ul>
                   {props.addr.businessaddrs.map((rba, idx) => <li key={idx}>{rba}</li>)}
@@ -33,7 +34,7 @@ const OwnersTable: React.FC<{
               )}
             </div>
             <div className="column col-3">
-              <p>Shell Companies</p>
+              <Trans render="p">Shell Companies</Trans>
               { props.addr.corpnames && props.addr.corpnames.length && (
                 <ul>
                   {props.addr.corpnames.map((corp, idx) => <li key={idx}>{corp}</li>)}
@@ -41,7 +42,7 @@ const OwnersTable: React.FC<{
               )}
             </div>
             <div className="column col-6">
-              <p>People</p>
+              <Trans render="p">People</Trans>
               { props.addr.ownernames && props.addr.ownernames.length && (
                 <ul className="owners">
                   {props.addr.ownernames.map((owner, idx) => <li key={idx}>{owner.title.split(/(?=[A-Z])/).join(" ")}: {owner.value}</li>)}
