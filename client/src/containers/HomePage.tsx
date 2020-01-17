df2f8bf2d53a0e98dc62bda6e431f7ac7aaafac8
Atul Varma <varmaa@gmail.com>
2020-01-17 06:36:57 -0500

    Translate HomePage.tsx.

--- client/src/containers/HomePage.tsx
+++ client/src/containers/HomePage.tsx
@@ -12,6 +12,7 @@ import westminsterLogo from '../assets/img/westminster.svg';
 import allyearLogo from '../assets/img/allyear.png';
 import aeLogo from '../assets/img/aande.jpeg';
 import AddressSearch, { makeEmptySearchAddress, SearchAddress } from '../components/AddressSearch';
+import { Trans } from '@lingui/macro';
 
 type HomePageProps = {};
 
@@ -96,14 +97,14 @@ class HomePage extends Component<HomePageProps, State> {
       );
     }
 
-    const labelText = "Enter an NYC address and find other buildings your landlord might own:";
+    const labelText = <Trans>Enter an NYC address and find other buildings your landlord might own:</Trans>;
 
     return (
       <div className="HomePage Page">
         <div className="HomePage__content">
           <div className="HomePage__search">
             { this.state.searchAddress.housenumber ? (
-              <Loader loading={true} >Searching for <b>{this.state.searchAddress.housenumber} {this.state.searchAddress.streetname}, {this.state.searchAddress.boro}</b></Loader>
+              <Loader loading={true} ><Trans>Searching for <b>{this.state.searchAddress.housenumber} {this.state.searchAddress.streetname}, {this.state.searchAddress.boro}</b></Trans></Loader>
             ) : (
               <div>
                 <h5 className="text-center">{labelText}</h5>
@@ -117,56 +118,56 @@ class HomePage extends Component<HomePageProps, State> {
             )}
           </div>
           <div className="HomePage__samples">
-            <h5 className="text-center">... or view some sample portfolios:</h5>
+            <h5 className="text-center"><Trans>... or view some sample portfolios:</Trans></h5>
             <div className="container">
               <div className="columns">
                 <div className="column col-4 col-sm-12">
                   <div className="HomePage__sample">
                     <h6>
                       <Link to={this.state.sampleURLs[0]} onClick={() => {window.gtag('event', 'example-portfolio-1-homepage');}} >
-                        Kushner Companies / Westminster Management
+                        <Trans>Kushner Companies / Westminster Management</Trans>
                       </Link>
                     </h6>
                     <Link to={this.state.sampleURLs[0]} onClick={() => {window.gtag('event', 'example-portfolio-1-homepage');}} >
                       <img className="img-responsive" src={westminsterLogo} alt="Westminster" />
                     </Link>
-                    <p>
+                    <Trans render="p">
                       This property management company owned by the Kushner family is notorious for <a href="https://www.nytimes.com/2017/08/15/business/tenants-sue-kushner-companies-claiming-rent-rule-violations.html" target="_blank" rel="noopener noreferrer">violating rent regulations</a> and <a href="https://www.villagevoice.com/2017/01/12/jared-kushners-east-village-tenants-horrified-their-landlord-will-be-working-in-the-white-house/" target="_blank" rel="noopener noreferrer">harassing tenants</a>. The stake currently held by Jared Kushner and Ivanka Trump is worth as much as $761 million.
-                    </p>
-                    <Link className="btn block text-center" to={this.state.sampleURLs[0]} onClick={() => {window.gtag('event', 'example-portfolio-1-homepage');}} >View portfolio &#10230;</Link>
+                    </Trans>
+                    <Link className="btn block text-center" to={this.state.sampleURLs[0]} onClick={() => {window.gtag('event', 'example-portfolio-1-homepage');}} ><Trans>View portfolio</Trans> &#10230;</Link>
                   </div>
                 </div>
                 <div className="column col-4 col-sm-12">
                   <div className="HomePage__sample">
                     <h6>
                       <Link to={this.state.sampleURLs[1]}  onClick={() => {window.gtag('event', 'example-portfolio-2-homepage');}} >
-                        A&E Real Estate
+                        <Trans>A&amp;E Real Estate</Trans>
                       </Link>
                     </h6>
                     <Link to={this.state.sampleURLs[1]} onClick={() => {window.gtag('event', 'example-portfolio-2-homepage');}} >
-                      <img className="emassoc img-responsive" src={aeLogo} alt="E&M Associates" />
+                      <img className="emassoc img-responsive" src={aeLogo} alt="A&amp;E Real Estate" />
                     </Link>
-                    <p>
+                    <Trans render="p">
                       The city’s fifth <a href="https://www.worstevictorsnyc.org/evictors-list/citywide" target="_blank" rel="noopener noreferrer">worst evictor</a> in 2018, A&E is a prime example of a landlord who engages in <a href="https://www.dnainfo.com/new-york/20170124/washington-heights/ae-real-estate-holdings-affordable-housing-nyc-tax-breaks/" target="_blank" rel="noopener noreferrer">aggressive eviction strategies</a> to displace low-income tenants. 
                       Besides lack of repairs and frivolous evictions in housing court, A&E has also been known to use <a href="https://hcr.ny.gov/faqs-major-capital-and-individual-apartment-improvements" target="blank" rel="noopener noreferrer">MCIs</a> to <a href="https://www.nytimes.com/interactive/2018/05/20/nyregion/nyc-affordable-housing.html" target="_blank" rel="noopener noreferrer">double rents</a> in rent-stabilized buildings.
-                    </p>
-                    <Link className="btn block text-center" to={this.state.sampleURLs[1]} onClick={() => {window.gtag('event', 'example-portfolio-2-homepage');}} >View portfolio &#10230;</Link>
+                    </Trans>
+                    <Link className="btn block text-center" to={this.state.sampleURLs[1]} onClick={() => {window.gtag('event', 'example-portfolio-2-homepage');}} ><Trans>View portfolio</Trans> &#10230;</Link>
                   </div>
                 </div>
                 <div className="column col-4 col-sm-12">
                   <div className="HomePage__sample">
                     <h6>
                       <Link to={this.state.sampleURLs[2]} onClick={() => {window.gtag('event', 'example-portfolio-1-homepage');}} >
-                        All Year Management
+                        <Trans>All Year Management</Trans>
                       </Link>
                     </h6>
                       <Link to={this.state.sampleURLs[2]} onClick={() => {window.gtag('event', 'example-portfolio-1-homepage');}} >
                         <img className="img-responsive" src={allyearLogo} alt="All Year" />
                       </Link>
-                    <p>
+                    <Trans render="p">
                       Yoel Goldman's All Year Management has been at the <a href="https://commercialobserver.com/2017/09/yoel-goldman-all-year-management-brooklyn-real-estate/" target="_blank" rel="noopener noreferrer">forefront of gentrification</a> in Brooklyn. Tenants in his buidlings in Williamsburg, Bushwick, and Crown Heights have been forced to live in horrendous and often dangerous conditions.
-                    </p>
-                    <Link className="btn block text-center" to={this.state.sampleURLs[2]} onClick={() => {window.gtag('event', 'example-portfolio-3-homepage');}} >View portfolio &#10230;</Link>
+                    </Trans>
+                    <Link className="btn block text-center" to={this.state.sampleURLs[2]} onClick={() => {window.gtag('event', 'example-portfolio-3-homepage');}} ><Trans>View portfolio</Trans> &#10230;</Link>
                   </div>
                 </div>
 
