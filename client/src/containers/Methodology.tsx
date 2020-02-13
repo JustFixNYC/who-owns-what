b2fffe2c3c6b09de35a95a130da1d031f663b638
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-02-13 15:12:51 -0500

    contentful update, fixing data source links

--- client/src/containers/Methodology.tsx
+++ client/src/containers/Methodology.tsx
@@ -4,6 +4,7 @@ import EngagementPanel from '../components/EngagementPanel';
 import en from '../data/how-it-works.en.json';
 import es from '../data/how-it-works.es.json';
 
+/* Use Same Styling as How to Use Page */
 import 'styles/HowToUsePage.css';
 import { ContentfulPage } from '../contentful/ContentfulPage';
 
