b8b66a03ea025cbb2f8b025aa86dbe94026c2a1a
Sam Raby <sraby@users.noreply.github.com>
2020-05-21 08:44:16 -0400

    Added most recent sale data to `create_bldgs_table` SQL code (#246)
    This PR adds three new columns to our wow_bldgs table: LASTSALEACRISID,
    LASTSALEDATE, and LASTSALEAMOUNT. These field values all correspond to the last non-trivial deed found in ACRIS for the associated BBL.
    
    Given that all ACRIS data is all grouped together as one dataset in nycdb, we needed to copy over test data for all acris tables, and then copy over the test acris data over verbatim into our temporary nycdb directory for testing. See tests/conftest.py for more details. Huge thanks to @toolness for walking through the madness that was figuring this out.

---
+++ tests/factories/real_property_master.py
@@ -0,0 +1,18 @@
+from typing import NamedTuple
+
+
+class RealPropertyMaster(NamedTuple):
+    documentid: str = 'FT_4990009008899'
+    recordtype: str = 'A'
+    crfn: str = ''
+    borough: str = '4'
+    doctype: str = 'MTGE'
+    docdate: str = ''
+    docamount: str = '170000'
+    recordedfiled: str = '2002-12-26'
+    modifieddate: str = '2003-02-28'
+    reelyear: str = '2002'
+    reelnbr: str = '6711'
+    reelpage: str = '2708'
+    pcttransferred: str = '0'
+    goodthroughdate: str = '2015-07-31'
