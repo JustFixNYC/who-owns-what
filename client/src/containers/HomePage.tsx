f78d91d4f1795c0934156b71ce105f11184b0375
Sam Raby <Sam@Sams-MacBook-Pro-5.local>
2020-05-28 15:36:14 -0400

    added back classnames and tabindex to images on home page

--- client/src/containers/HomePage.tsx
+++ client/src/containers/HomePage.tsx
@@ -198,6 +198,8 @@ class HomePage extends Component<HomePageProps, State> {
                         </Link>
                       </h6>
                       <Link
+                        className="image"
+                        tabIndex={-1} // Since link is not necessary navigation, removing tab focus
                         to={this.state.sampleURLs[0]}
                         onClick={() => {
                           window.gtag("event", "example-portfolio-1-homepage");
@@ -250,6 +252,8 @@ class HomePage extends Component<HomePageProps, State> {
                         </Link>
                       </h6>
                       <Link
+                        className="image"
+                        tabIndex={-1} // Since link is not necessary navigation, removing tab focus
                         to={this.state.sampleURLs[1]}
                         onClick={() => {
                           window.gtag("event", "example-portfolio-2-homepage");
@@ -321,6 +325,8 @@ class HomePage extends Component<HomePageProps, State> {
                         </Link>
                       </h6>
                       <Link
+                        className="image"
+                        tabIndex={-1} // Since link is not necessary navigation, removing tab focus
                         to={this.state.sampleURLs[2]}
                         onClick={() => {
                           window.gtag("event", "example-portfolio-1-homepage");
