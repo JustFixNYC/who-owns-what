97cdb36b13067775354dd291ef416c4d94fa0852
Sam R <sam@MacBook-Pro.local>
2020-07-14 20:01:22 -0400

    Separated out paths from routes component

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -9,41 +9,62 @@ import MethodologyPage from "./containers/Methodology";
 import TermsOfUsePage from "./containers/TermsOfUsePage";
 import PrivacyPolicyPage from "./containers/PrivacyPolicyPage";
 
-export const localizeRoute = (route: string) => "/:locale" + route;
-
 export const createRouteForAddressPage = (boro: string, streetname: string, housenumber?: string) =>
   `/address/${boro}/${housenumber || " "}/${streetname}`;
 
-const addressPageRouteWithParams = localizeRoute(
-  createRouteForAddressPage(":boro", ":streetname", ":housenumber")
+const addressPageRouteWithParams = createRouteForAddressPage(
+  ":boro",
+  ":streetname",
+  ":housenumber"
 );
 
-export const WhoOwnsWhatRoutes = () => (
-  <Switch>
-    <Route exact path={localizeRoute("/")} component={HomePage} />
-    <Route
-      path={addressPageRouteWithParams}
-      render={(props) => <AddressPage currentTab={0} {...props} />}
-      exact
-    />
-    <Route
-      path={addressPageRouteWithParams + "/timeline"}
-      render={(props) => <AddressPage currentTab={1} {...props} />}
-    />
-    <Route
-      path={addressPageRouteWithParams + "/portfolio"}
-      render={(props) => <AddressPage currentTab={2} {...props} />}
-    />
-    <Route
-      path={addressPageRouteWithParams + "/summary"}
-      render={(props) => <AddressPage currentTab={3} {...props} />}
-    />
-    <Route path={localizeRoute("/bbl/:boro/:block/:lot")} component={BBLPage} />
-    <Route path={localizeRoute("/bbl/:bbl")} component={BBLPage} />
-    <Route path={localizeRoute("/about")} component={AboutPage} />
-    <Route path={localizeRoute("/how-to-use")} component={HowToUsePage} />
-    <Route path={localizeRoute("/how-it-works")} component={MethodologyPage} />
-    <Route path={localizeRoute("/terms-of-use")} component={TermsOfUsePage} />
-    <Route path={localizeRoute("/privacy-policy")} component={PrivacyPolicyPage} />
-  </Switch>
-);
+const createWhoOwnsWhatRoutePaths = (prefix?: string) => {
+  const pathPrefix = prefix || "";
+  return {
+    home: `${pathPrefix}/`,
+    addressPageOverview: `${pathPrefix}${addressPageRouteWithParams}`,
+    addressPageTimeline: `${pathPrefix}${addressPageRouteWithParams}/timeline`,
+    addressPagePortfolio: `${pathPrefix}${addressPageRouteWithParams}/portfolio`,
+    addressPageSummary: `${pathPrefix}${addressPageRouteWithParams}/summary`,
+    bbl: `${pathPrefix}/bbl/:boro/:block/:lot`,
+    bblWithFullBblInUrl: `${pathPrefix}/bbl/:bbl`,
+    about: `${pathPrefix}/about`,
+    howToUse: `${pathPrefix}/how-to-use`,
+    methodology: `${pathPrefix}/how-it-works`,
+    termsOfUse: `${pathPrefix}/terms-of-use`,
+    privacyPolicy: `${pathPrefix}/privacy-policy`,
+  };
+};
+
+export const WhoOwnsWhatRoutes = () => {
+  const paths = createWhoOwnsWhatRoutePaths("/:locale");
+  return (
+    <Switch>
+      <Route exact path={paths.home} component={HomePage} />
+      <Route
+        path={paths.addressPageOverview}
+        render={(props) => <AddressPage currentTab={0} {...props} />}
+        exact
+      />
+      <Route
+        path={paths.addressPageTimeline}
+        render={(props) => <AddressPage currentTab={1} {...props} />}
+      />
+      <Route
+        path={paths.addressPagePortfolio}
+        render={(props) => <AddressPage currentTab={2} {...props} />}
+      />
+      <Route
+        path={paths.addressPageSummary}
+        render={(props) => <AddressPage currentTab={3} {...props} />}
+      />
+      <Route path={paths.bbl} component={BBLPage} />
+      <Route path={paths.bblWithFullBblInUrl} component={BBLPage} />
+      <Route path={paths.about} component={AboutPage} />
+      <Route path={paths.howToUse} component={HowToUsePage} />
+      <Route path={paths.methodology} component={MethodologyPage} />
+      <Route path={paths.termsOfUse} component={TermsOfUsePage} />
+      <Route path={paths.privacyPolicy} component={PrivacyPolicyPage} />
+    </Switch>
+  );
+};
