f4f88f2ff78376691afd2ebe2bc006cef65d762f
Atul Varma <varmaa@gmail.com>
2020-07-20 09:03:19 -0400

    Load test WoW data for view tests.

--- wow/tests/test_views.py
+++ wow/tests/test_views.py
@@ -1,4 +1,18 @@
+import pytest
+
+
 def test_hello_works(client):
     res = client.get('/api/hello')
     assert res.status_code == 200
     assert res.content == b"hello world"
+
+
+def test_address_query(db, client):
+    res = client.get('/api/address?block=01678&lot=0054&borough=3')
+    assert res.status_code == 200
+    json = res.json()
+    assert len(json['addrs']) > 0
+    assert json['geosearch'] == {
+        'bbl': '3016780054',
+        'geosupportReturnCode': '00',
+    }
