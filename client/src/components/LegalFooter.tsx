fa404a82b13a33f8606a01563f215ff59578284b
Sam Rabiyah <sraby@users.noreply.github.com>
2020-07-22 13:30:28 -0400

    Add netlify banner to footer (#310)
    This PR adds a banner to our footer according to Netlify's Open Source Policy.

--- client/src/components/LegalFooter.tsx
+++ client/src/components/LegalFooter.tsx
@@ -57,6 +57,18 @@ class LegalFooter extends Component {
                   <Trans>Source code</Trans>
                 </a>
               </nav>
+              <a
+                href="https://www.netlify.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hide-md"
+              >
+                <img
+                  src="https://www.netlify.com/img/global/badges/netlify-dark.svg"
+                  alt="Netlify"
+                  width="75"
+                />
+              </a>
             </div>
           </div>
         </div>
