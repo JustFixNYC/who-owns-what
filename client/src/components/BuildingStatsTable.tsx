54d3350d76e77cb3eed7b0670fa5c50c4c43b8a6
Sam R <sam@MacBook-Pro.local>
2020-07-28 10:24:55 -0400

    Finished adding ts to Building Stats table

--- client/src/components/BuildingStatsTable.tsx
+++ client/src/components/BuildingStatsTable.tsx
@@ -7,22 +7,6 @@ import { withI18n, I18n } from "@lingui/react";
 import { t } from "@lingui/macro";
 import { Trans } from "@lingui/macro";
 
-type Props = {
-  addr: AddressRecord;
-  i18n: I18n;
-};
-
-const EmptyBuildingStatsData = {
-  bbl: "",
-  yearbuilt: 0,
-  unitsres: 0,
-  openviolations: 0,
-  totalviolations: 0,
-  evictions: null,
-  rsunits2007: null,
-  rsunits2017: null,
-};
-
 interface BuildingStatsAddrContext {
   getBuildingStats(): AddressRecord;
 }
@@ -32,7 +16,6 @@ const AddrContext = React.createContext<BuildingStatsAddrContext>({
     throw new Error("This Building Stats table has no data!");
   },
 });
-const I18nContext = React.createContext({});
 
 const bblDash = (
   <span className="unselectable" unselectable="on">
@@ -43,167 +26,185 @@ const bblDash = (
 const BBL = () => {
   const addr = useContext(AddrContext).getBuildingStats();
   const { boro, block, lot } = Helpers.splitBBL(addr.bbl);
-  const i18n = useContext(I18nContext);
   return (
-    <div
-      className="double"
-      title={i18n._(
-        t`This is the official identifer for the building according to the Dept. of Finance tax records.`
+    <I18n>
+      {({ i18n }) => (
+        <div
+          className="double"
+          title={i18n._(
+            t`This is the official identifer for the building according to the Dept. of Finance tax records.`
+          )}
+        >
+          <label>
+            Boro{bblDash}Block{bblDash}Lot (BBL)
+          </label>
+          {boro}
+          {bblDash}
+          {block}
+          {bblDash}
+          {lot}
+        </div>
       )}
-    >
-      <label>
-        Boro{bblDash}Block{bblDash}Lot (BBL)
-      </label>
-      {boro}
-      {bblDash}
-      {block}
-      {bblDash}
-      {lot}
-    </div>
+    </I18n>
   );
 };
 
 const YearBuilt = () => {
   const addr = useContext(AddrContext).getBuildingStats();
-  const i18n = useContext(I18nContext);
   return (
-    <div
-      title={i18n._(
-        t`The year that this building was originally constructed, according to the Dept. of City Planning.`
+    <I18n>
+      {({ i18n }) => (
+        <div
+          title={i18n._(
+            t`The year that this building was originally constructed, according to the Dept. of City Planning.`
+          )}
+        >
+          <Trans render="label">Year Built</Trans>
+          {addr.yearbuilt !== 0 ? addr.yearbuilt : "N/A"}
+        </div>
       )}
-    >
-      <Trans render="label">Year Built</Trans>
-      {addr.yearbuilt !== 0 ? addr.yearbuilt : "N/A"}
-    </div>
+    </I18n>
   );
 };
 
 const UnitsRes = () => {
   const addr = useContext(AddrContext).getBuildingStats();
-  const i18n = useContext(I18nContext);
   return (
-    <div
-      title={i18n._(
-        t`The number of residential units in this building, according to the Dept. of City Planning.`
+    <I18n>
+      {({ i18n }) => (
+        <div
+          title={i18n._(
+            t`The number of residential units in this building, according to the Dept. of City Planning.`
+          )}
+        >
+          <Trans render="label">Units</Trans>
+          {addr.unitsres}
+        </div>
       )}
-    >
-      <Trans render="label">Units</Trans>
-      {addr.unitsres}
-    </div>
+    </I18n>
   );
 };
 
 const OpenViolations = () => {
   const addr = useContext(AddrContext).getBuildingStats();
-  const i18n = useContext(I18nContext);
   return (
-    <div
-      title={i18n._(
-        t`The number of open HPD violations for this building, updated monthly. Click the HPD Building Profile button below for the most up-to-date information.`
+    <I18n>
+      {({ i18n }) => (
+        <div
+          title={i18n._(
+            t`The number of open HPD violations for this building, updated monthly. Click the HPD Building Profile button below for the most up-to-date information.`
+          )}
+        >
+          <Trans render="label">Open Violations</Trans>
+          {addr.openviolations}
+        </div>
       )}
-    >
-      <Trans render="label">Open Violations</Trans>
-      {addr.openviolations}
-    </div>
+    </I18n>
   );
 };
 
 const TotalViolations = () => {
   const addr = useContext(AddrContext).getBuildingStats();
-  const i18n = useContext(I18nContext);
   return (
-    <div
-      title={i18n._(
-        t`This represents the total number of HPD Violations (both open & closed) recorded by the city.`
+    <I18n>
+      {({ i18n }) => (
+        <div
+          title={i18n._(
+            t`This represents the total number of HPD Violations (both open & closed) recorded by the city.`
+          )}
+        >
+          <Trans render="label">Total Violations</Trans>
+          {addr.totalviolations}
+        </div>
       )}
-    >
-      <Trans render="label">Total Violations</Trans>
-      {addr.totalviolations}
-    </div>
+    </I18n>
   );
 };
 
 const Evictions = () => {
   const addr = useContext(AddrContext).getBuildingStats();
-  const i18n = useContext(I18nContext);
   return (
-    <div
-      title={i18n._(
-        t`Evictions executed by NYC Marshals in 2019. ANHD and the Housing Data Coalition cleaned, geocoded, and validated the data, originally sourced from DOI.`
+    <I18n>
+      {({ i18n }) => (
+        <div
+          title={i18n._(
+            t`Evictions executed by NYC Marshals in 2019. ANHD and the Housing Data Coalition cleaned, geocoded, and validated the data, originally sourced from DOI.`
+          )}
+        >
+          <Trans render="label">2019 Evictions</Trans>
+          {addr.evictions !== null ? addr.evictions : "N/A"}
+        </div>
       )}
-    >
-      <Trans render="label">2019 Evictions</Trans>
-      {addr.evictions !== null ? addr.evictions : "N/A"}
-    </div>
+    </I18n>
   );
 };
 
 const RsUnits = () => {
   const addr = useContext(AddrContext).getBuildingStats();
-  const i18n = useContext(I18nContext);
   return (
-    <div
-      title={i18n._(
-        t`This tracks how rent stabilized units in the building have changed (i.e. "&Delta;") from 2007 to 2017. If the number for 2017 is red, this means there has been a loss in stabilzied units! These counts are estimated from the DOF Property Tax Bills.`
+    <I18n>
+      {({ i18n }) => (
+        <div
+          title={i18n._(
+            t`This tracks how rent stabilized units in the building have changed (i.e. "&Delta;") from 2007 to 2017. If the number for 2017 is red, this means there has been a loss in stabilzied units! These counts are estimated from the DOF Property Tax Bills.`
+          )}
+        >
+          <label>
+            &Delta; <Trans>RS Units</Trans>
+          </label>
+          <span>{addr.rsunits2007 !== null ? addr.rsunits2007 : "N/A"}</span>
+          <span>&#x21FE;</span>
+          <span
+            className={`${
+              addr.rsunits2007 && addr.rsunits2017 && addr.rsunits2017 < addr.rsunits2007
+                ? "text-danger"
+                : ""
+            }`}
+          >
+            {addr.rsunits2017 !== null ? addr.rsunits2017 : "N/A"}
+          </span>
+        </div>
       )}
-    >
-      <label>
-        &Delta; <Trans>RS Units</Trans>
-      </label>
-      <span>{addr.rsunits2007 !== null ? addr.rsunits2007 : "N/A"}</span>
-      <span>&#x21FE;</span>
-      <span
-        className={`${
-          addr.rsunits2007 && addr.rsunits2017 && addr.rsunits2017 < addr.rsunits2007
-            ? "text-danger"
-            : ""
-        }`}
-      >
-        {addr.rsunits2017 !== null ? addr.rsunits2017 : "N/A"}
-      </span>
-    </div>
+    </I18n>
   );
 };
 
-const BuildingStatsTableWithoutI18n = (props: Props) => (
+const BuildingStatsTableWithoutI18n = (props: { addr: AddressRecord }) => (
   <AddrContext.Provider
     value={{
       getBuildingStats: () => props.addr,
     }}
   >
-    <I18nContext.Provider value={props.i18n}>
-      <div className="card-body-table hide-sm">
-        <div className="table-row">
-          <BBL />
-          <YearBuilt />
-          <UnitsRes />
-        </div>
-        <div className="table-row">
-          <OpenViolations />
-          <TotalViolations />
-          <Evictions />
-          <RsUnits />
-        </div>
+    <div className="card-body-table hide-sm">
+      <div className="table-row">
+        <BBL />
+        <YearBuilt />
+        <UnitsRes />
       </div>
-      <div className="card-body-table show-sm">
-        <div className="table-row">
-          <BBL />
-        </div>
-        <div className="table-row">
-          <YearBuilt />
-          <UnitsRes />
-          <RsUnits />
-        </div>
-        <div className="table-row">
-          <OpenViolations />
-          <TotalViolations />
-          <Evictions />
-        </div>
+      <div className="table-row">
+        <OpenViolations />
+        <TotalViolations />
+        <Evictions />
+        <RsUnits />
       </div>
-      <span className="card-body-table-prompt float-right">
-        <Trans render="i">(hover over a box to learn more)</Trans>
-      </span>
-    </I18nContext.Provider>
+    </div>
+    <div className="card-body-table show-sm">
+        <BBL />
+      </div>
+      <div className="table-row">
+        <YearBuilt />
+        <UnitsRes />
+        <RsUnits />
+      </div>
+      <div className="table-row">
+        <OpenViolations />
+        <TotalViolations />
+        <Evictions />
+      </div>
+    </div>
+    <span className="card-body-table-prompt float-right">
+      <Trans render="i">(hover over a box to learn more)</Trans>
+    </span>
   </AddrContext.Provider>
 );
 
