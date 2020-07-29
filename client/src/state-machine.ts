070847b2ae62daff5dca8d5192e458557147cea4
Sam R <sam@MacBook-Pro.local>
2020-07-29 15:50:40 -0400

    clarified machine context structure

--- client/src/state-machine.ts
+++ client/src/state-machine.ts
@@ -13,24 +13,28 @@ type WowEvent =
   | { type: "VIEW_SUMMARY" }
   | { type: "VIEW_TIMELINE" };
 
+type PortfolioData = {
+  /** The full set of data for the original search address */
+  searchAddr: AddressRecord;
+  /**
+   * All associated addresses connected to the search address
+   * found in our database of HPD registered buildings
+   */
+  assocAddrs: AddressRecord[];
+  /** The address in focus on the Overview Tab and Timeline Tab */
+  detailAddr: AddressRecord;
+};
+
 interface WowContext {
   /** The original parameters that a user inputs to locate their building on WOW */
   searchAddrParams?: SearchAddressWithoutBbl;
   /** The BBL code found by GeoSearch corresponding with the search address parameters */
   searchAddrBbl?: string;
   /**
-   * The full set of data for the search address,
-   * retrieved ONLY if the address's bbl has a matching record
-   * in our database of HPD registered buildings
+   * All data regarding the portfolio of buildings associated with the search address
+   * retrieved ONLY if the address's bbl has a matching record in our database of HPD registered buildings
    */
-  searchAddr?: AddressRecord;
-  /**
-   * All associated addresses connected to the search address
-   * found in our database of HPD registered buildings
-   */
-  assocAddrs?: AddressRecord[];
-  /** The address in focus on the Overview Tab and Timeline Tab */
-  detailAddr?: AddressRecord;
+  portfolioData?: PortfolioData;
 }
 
 const wowMachine = Machine<WowContext, WowStateSchema, WowEvent>({
