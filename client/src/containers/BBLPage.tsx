0ff61268a01699d1fdb25f6d2c02a1a2e51cf847
Atul Varma <varmaa@gmail.com>
2020-07-30 16:30:30 -0400

    Show 'no address found' page on BBL route.

--- client/src/containers/BBLPage.tsx
+++ client/src/containers/BBLPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Loader from "../components/Loader";
 import APIClient from "../components/APIClient";
@@ -8,6 +8,7 @@ import Page from "../components/Page";
 import { createRouteForAddressPage } from "../routes";
 import { makeEmptySearchAddress } from "../components/AddressSearch";
 import { SearchAddressWithoutBbl } from "../components/APIDataTypes";
+import NotFoundPage from "./NotFoundPage";
 
 // This will be *either* bbl *or* boro, block, and lot.
 type BBLPageParams = {
@@ -22,6 +23,7 @@ type BBLPageProps = RouteComponentProps<BBLPageParams>;
 const BBLPage: React.FC<BBLPageProps> = (props) => {
   const history = useHistory();
   const params = props.match.params;
+  const [isNotFound, setIsNotFound] = useState(false);
 
   var fullBBL: string;
   var searchAddress: SearchAddressWithoutBbl = makeEmptySearchAddress();
@@ -35,21 +37,36 @@ const BBLPage: React.FC<BBLPageProps> = (props) => {
     throw new Error("Invalid params, expected either a BBL or boro/block/lot!");
   }
 
-  APIClient.getBuildingInfo(fullBBL)
-    .then((results) => {
-      searchAddress = {
-        boro: results.result[0].boro,
-        housenumber: results.result[0].housenumber,
-        streetname: results.result[0].streetname,
-      };
-      const addressPage = createRouteForAddressPage(searchAddress);
-      history.push(addressPage);
-    })
-    .catch((err) => {
-      window.Rollbar.error("API error from BBL page: Building Info", err, fullBBL);
-    });
+  useEffect(() => {
+    let isMounted = true;
 
-  return (
+    APIClient.getBuildingInfo(fullBBL)
+      .then((results) => {
+        if (!isMounted) return;
+        if (results.result.length === 0) {
+          setIsNotFound(true);
+          return;
+        }
+        searchAddress = {
+          boro: results.result[0].boro,
+          housenumber: results.result[0].housenumber,
+          streetname: results.result[0].streetname,
+        };
+        const addressPage = createRouteForAddressPage(searchAddress);
+        history.push(addressPage);
+      })
+      .catch((err) => {
+        window.Rollbar.error("API error from BBL page: Building Info", err, fullBBL);
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return isNotFound ? (
+    <NotFoundPage />
+  ) : (
     <Page>
       <Loader loading={true} classNames="Loader-map">
         <Trans>Loading</Trans>
