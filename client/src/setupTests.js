dfbadb6ab9bf2bba00385a181b894f819cbc2b53
Sam R <sam@MacBook-Pro.local>
2020-08-05 17:17:36 -0400

    Added test for bbl page params formatting fn

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -7,3 +7,7 @@ window.Rollbar = {
 };
 
 require("jest-fetch-mock").enableMocks();
+
+// We need to "mock" this function here as Jest doesn't work well with MapBox GL.
+// Read more here: https://github.com/mapbox/mapbox-gl-js/issues/3436#issuecomment-421117409
+window.URL.createObjectURL = function () {};
