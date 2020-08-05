d88b796a15ffc1567983bc9b0e5b6ce4e962ffa6
Sam R <sam@MacBook-Pro.local>
2020-08-05 16:34:28 -0400

    reached full test coverage of helpers.ts

--- client/src/util/helpers.test.ts
+++ client/src/util/helpers.test.ts
@@ -1,4 +1,39 @@
-import helpers from "./helpers";
+import helpers, { searchAddrsAreEqual, assertNotUndefined } from "./helpers";
+import { SearchAddressWithoutBbl } from "components/APIDataTypes";
+
+describe("assertNotUndefined()", () => {
+  it("raises exception when undefined", () => {
+    expect(() => assertNotUndefined(undefined)).toThrowError(
+      "expected argument to not be undefined"
+    );
+  });
+
+  it("returns argument when not undefined", () => {
+    expect(assertNotUndefined(null)).toBe(null);
+  });
+});
+
+describe("searchAddrsAreEqual()", () => {
+  const searchAddr1: SearchAddressWithoutBbl = {
+    boro: "BRONX",
+    streetname: "BWAY",
+    housenumber: "1",
+  };
+
+  const searchAddr2: SearchAddressWithoutBbl = {
+    boro: "MANHATTAN",
+    streetname: "SEWER",
+  };
+  it("returns true when addrs are equal", () => {
+    expect(searchAddrsAreEqual(searchAddr1, searchAddr1)).toBe(true);
+  });
+  it("still works if housenumber is not defined", () => {
+    expect(searchAddrsAreEqual(searchAddr2, searchAddr2)).toBe(true);
+  });
+  it("returns false when addrs are different", () => {
+    expect(searchAddrsAreEqual(searchAddr1, searchAddr2)).toBe(false);
+  });
+});
 
 test("uniq() works", () => {
   expect(helpers.uniq([1, 1, 2, 4, 4])).toEqual([1, 2, 4]);
@@ -53,6 +88,23 @@ describe("jsonEqual()", () => {
   });
 });
 
+describe("formatPrice()", () => {
+  it("works with no locale provided", () => {
+    expect(helpers.formatPrice(1000000)).toBe("1,000,000");
+  });
+  it("works with specified locales", () => {
+    expect(helpers.formatPrice(1000000, "es")).toBe("1.000.000");
+  });
+});
+
+test("createTakeActionURL() works", () => {
+  expect(
+    helpers.createTakeActionURL({ boro: "QUEENS", streetname: "BOOP RD", housenumber: "1" }, "boop")
+  ).toBe(
+    "https://app.justfix.nyc/ddo?address=1%20BOOP%20RD&borough=QUEENS&utm_source=whoownswhat&utm_content=take_action&utm_medium=boop"
+  );
+});
+
 test("intersectAddrObjects() works", () => {
   expect(helpers.intersectAddrObjects({ a: 1, b: 2, c: 3, d: 9 }, { a: 5, b: 2, c: 9 })).toEqual({
     b: 2,
@@ -67,49 +119,64 @@ test("titleCase() works", () => {
   expect(helpers.titleCase("boop jones")).toBe("Boop Jones");
 });
 
-test("formatDate() works", () => {
-  expect(helpers.formatDate("2008-01-05", { year: "numeric", month: "long" })).toBe("January 2008");
+describe("formatDate()", () => {
+  it("works", () => {
+    expect(helpers.formatDate("2008-01-05", { year: "numeric", month: "long" })).toBe(
+      "January 2008"
+    );
+  });
+
+  it("works for month abbreviation", () => {
+    expect(helpers.formatDate("2008-01-05", { month: "short" })).toBe("Jan");
+  });
+
+  // Note: Although there is generally good support across modern versions of the usual web browsers,
+  // the "locale" prop in the toLocaleDateString function is not supported until Node 13.
+  // Therefore, I implemented these two tests to match either the localized result or the default.
+  // See more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+
+  it("works for non-English locales", () => {
+    expect(helpers.formatDate("2008-01-05", { year: "numeric", month: "long" }, "es")).toMatch(
+      /Enero de 2008|January 2008/i
+    );
+  });
+
+  it("works for month abbreviation in non-English locales", () => {
+    expect(helpers.formatDate("2008-01-05", { month: "short" }, "es")).toMatch(/Ene|Jan/i);
+  });
 });
 
-test("formatDate() works for month abbreviation", () => {
-  expect(helpers.formatDate("2008-01-05", { month: "short" })).toBe("Jan");
+describe("getMonthRangeFromQuarter()", () => {
+  it("works with no locale provided", () => {
+    expect(helpers.getMonthRangeFromQuarter("1")).toBe("Jan - Mar");
+  });
+  it("works with specified locales", () => {
+    expect(helpers.getMonthRangeFromQuarter("1", "es")).toBe("Ene - Mar");
+  });
 });
 
-// Note: Although there is generally good support across modern versions of the usual web browsers,
-// the "locale" prop in the toLocaleDateString function is not supported until Node 13.
-// Therefore, I implemented these two tests to match either the localized result or the default.
-// See more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+describe("formatStreetNameForHpdLink()", () => {
+  it("works for directional prefixes", () => {
+    expect(helpers.formatStreetNameForHpdLink("East 21st Street")).toBe("E 21st Street");
+  });
 
-test("formatDate() works for  non-English locales", () => {
-  expect(helpers.formatDate("2008-01-05", { year: "numeric", month: "long" }, "es")).toMatch(
-    /Enero de 2008|January 2008/i
-  );
-});
+  it("works for enumerations", () => {
+    expect(helpers.formatStreetNameForHpdLink("Eighth Avenue")).toBe("8 Avenue");
+  });
 
-test("formatDate() works for timeline in non-English locales", () => {
-  expect(helpers.formatDate("2008-01-05", { month: "short" }, "es")).toMatch(/Ene|Jan/i);
-});
+  it("works for both prefixes and enumerations at once", () => {
+    expect(helpers.formatStreetNameForHpdLink("North Second Street")).toBe("N 2 Street");
+  });
 
-test("formatStreetNameForHpdLink() works for directional prefixes", () => {
-  expect(helpers.formatStreetNameForHpdLink("East 21st Street")).toBe("E 21st Street");
-});
+  it("doesn't change directionals or enumerations within names", () => {
+    expect(helpers.formatStreetNameForHpdLink("Eastonfirst Avenue")).toBe("Eastonfirst Avenue");
+  });
 
-test("formatStreetNameForHpdLink() works for enumerations", () => {
-  expect(helpers.formatStreetNameForHpdLink("Eighth Avenue")).toBe("8 Avenue");
-});
+  it("still works for one-word streetnames", () => {
+    expect(helpers.formatStreetNameForHpdLink("Broadway")).toBe("Broadway");
+  });
 
-test("formatStreetNameForHpdLink() works for both prefixes and enumerations at once", () => {
-  expect(helpers.formatStreetNameForHpdLink("North Second Street")).toBe("N 2 Street");
-});
-
-test("formatStreetNameForHpdLink() doesn't change directionals or enumerations within names", () => {
-  expect(helpers.formatStreetNameForHpdLink("Eastonfirst Avenue")).toBe("Eastonfirst Avenue");
-});
-
-test("formatStreetNameForHpdLink() still works for one-word streetnames", () => {
-  expect(helpers.formatStreetNameForHpdLink("Broadway")).toBe("Broadway");
-});
-
-test("formatStreetNameForHpdLink() still works for empty streetnames", () => {
-  expect(helpers.formatStreetNameForHpdLink("")).toBe("");
+  it("still works for empty streetnames", () => {
+    expect(helpers.formatStreetNameForHpdLink("")).toBe("");
+  });
 });
