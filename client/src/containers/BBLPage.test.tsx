ca03fc39d93a7f7b351f287ee1277373b9b6265d
Sam Rabiyah <sraby@users.noreply.github.com>
2020-08-05 18:14:06 -0400

    Added test for the bbl page params formatting function (#375)
    This PR adds tests for our BBLPage component, which, for now, just consists of one test that checks whether we are appropriately reformatting the URL params that the component receives. This specific test makes sure that both url formats for our BBL direct links work— /bbl/4015640058 and /bbl/4/1564/58, for example.

---
+++ client/src/containers/BBLPage.test.tsx
@@ -0,0 +1,32 @@
+import { getFullBblFromPageParams, BBLPageParams } from "./BBLPage";
+
+const pageParamsWithFullBbl: BBLPageParams = {
+  bbl: "1003450021",
+};
+
+const pageParamsWithBblBits: BBLPageParams = {
+  boro: "2",
+  block: "43",
+  lot: "1",
+};
+
+const incompletePageParams: BBLPageParams = {
+  boro: "3",
+  lot: "1",
+};
+
+describe("getFullBblFromPageParams()", () => {
+  it("returns true when objects are equal", () => {
+    expect(getFullBblFromPageParams(pageParamsWithFullBbl)).toBe("1003450021");
+  });
+
+  it("returns false when objects are not equal", () => {
+    expect(getFullBblFromPageParams(pageParamsWithBblBits)).toBe("2000430001");
+  });
+
+  it("throws error if params are incomplete", () => {
+    expect(() => getFullBblFromPageParams(incompletePageParams)).toThrowError(
+      "Invalid params, expected either a BBL or boro/block/lot!"
+    );
+  });
+});
