9fa0561955f3ecde9effdd0c8092113b9d5acd9a
Atul Varma <varmaa@gmail.com>
2020-08-10 07:59:58 -0400

    Oops, imported the wrong Trans.

--- client/src/components/NetworkErrorMessage.tsx
+++ client/src/components/NetworkErrorMessage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ErrorPageScaffolding } from "containers/NotFoundPage";
-import { Trans } from "@lingui/react";
+import { Trans } from "@lingui/macro";
 
 export const NetworkErrorMessage: React.FC<{}> = () => (
   <ErrorPageScaffolding>
