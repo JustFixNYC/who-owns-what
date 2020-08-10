8cb8a3f641113c717ce21d6ae35dd67da0b7c0ec
Sydney Jones <47163893+Sjones21@users.noreply.github.com>
2020-08-10 10:09:00 -0700

    visual fixes
    modified CSS colors and margins

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -42,28 +42,29 @@ export default class App extends Component {
       entryIndex: 0,
       entries: [
         {
-          index: "1 of 4", 
+          index: "1 of 4",
           title: "Timeline Tab",
           description: "Click the Timeline tab to view info about your building over time",
-          img: Timeline_gif
+          img: Timeline_gif,
         },
         {
-          index: "2 of 4", 
+          index: "2 of 4",
           title: "Spanish Support",
-          description: "Click “ES” in the upper right corner to switch your view of Who Owns What to Spanish",
-          img: Spanish_gif
+          description:
+            "Click “ES” in the upper right corner to switch your view of Who Owns What to Spanish",
+          img: Spanish_gif,
         },
         {
-          index: "3 of 4", 
+          index: "3 of 4",
           title: "Unique Tab URLs",
-          description: "You can now share links to specific tabs",
-          img: URLS_gif
+          description: "It's now possible to share links to specific tabs (Overview, Timeline, Portfolio, & Summary)",
+          img: URLS_gif,
         },
         {
-          index: "4 of 4", 
+          index: "4 of 4",
           title: "Last Sold",
           description: "View the date and price from the last time your building was sold",
-          img: LastSold_gif
+          img: LastSold_gif,
         },
       ],
     };
@@ -83,20 +84,20 @@ export default class App extends Component {
       : this.setState({ entryIndex: this.state.entryIndex - 1 });
   }
 
-  toggleWidget(event, widget){
-    if (widget.style.display === "none"){
-    widget.style.display = "inline-block";
-    console.log(event.target);
-    //event.target.classList.add("toggled");
-    document.querySelector('.tooltip-triangle').classList.add('toggled');
-    document.querySelector('.button-info').classList.add('pressed');
+  toggleWidget(event, widget) {
+    if (widget.style.display === "none") {
+      widget.style.display = "inline-block";
+      console.log(event.target);
+      //event.target.classList.add("toggled");
+      document.querySelector(".tooltip-triangle").classList.add("toggled");
+      document.querySelector(".button-info").classList.add("pressed");
     } else {
-    widget.style.display = "none";
-    //event.target.classList.remove("toggled");
-    document.querySelector('.button-info').classList.remove('pressed');
-    document.querySelector('.tooltip-triangle').classList.remove('toggled');
+      widget.style.display = "none";
+      //event.target.classList.remove("toggled");
+      document.querySelector(".button-info").classList.remove("pressed");
+      document.querySelector(".tooltip-triangle").classList.remove("toggled");
+    }
   }
-}
 
   render() {
     const isDemoSite = process.env.REACT_APP_DEMO_SITE === "1";
@@ -104,39 +105,43 @@ export default class App extends Component {
 
     let widgetHeader = (
       <div className="header">
-        <span className="widget-title focusable" role = "heading" tabIndex="0">
+        <span className="widget-title focusable" role="heading" tabIndex="0">
           What's New
         </span>
         <button
           type="button"
           role="button"
-          className="button-cancel material-icons md-18 focusable" 
-          tabIndex = "0"
+          className="button-cancel material-icons md-18 focusable"
+          tabIndex="0"
           onClick={(event) => {
             event.preventDefault();
             let widget = document.getElementById("widget");
             return this.toggleWidget(event, widget);
           }}
         >
-            close
+          close
         </button>
       </div>
     );
 
     let entryDivs = this.state.entries.map((entry) => (
       <div
-        className = "entry"
+        className="entry"
         id={"page-" + entry.title}
         role="region"
         aria-labelledby={"widget-" + entry.title + "-id"}
       >
-        <p className = "entry-title-container">
-        <span className = "entry-index" aria-describedby={entry.index}>{entry.index}</span>
-        <span className = "entry-title" aria-describedby={"entry title- " + entry.title}>{entry.title}</span>
+        <p className="entry-title-container">
+          <span className="entry-index" aria-describedby={entry.index}>
+            {entry.index}
+          </span>
+          <span className="entry-title" aria-describedby={"entry title- " + entry.title}>
+            {entry.title}
+          </span>
         </p>
-        
-        <img className = "widget-image"src = {entry.img}></img>
-        <p className = "widget-description">{entry.description}</p>
+
+        <img className="widget-image" src={entry.img}></img>
+        <p className="widget-description">{entry.description}</p>
       </div>
     ));
 
@@ -144,55 +149,44 @@ export default class App extends Component {
       <div className="navButtons">
         <button
           className="button-nav prev focusable"
-          tabIndex = "0"
+          tabIndex="0"
           onClick={(event) => {
             event.preventDefault();
             return this.prevEntry(event);
           }}
         >
-        <span 
-        class="material-icons md-14 prev-next-icon"
-        >
-          navigate_before
-        </span>
-        <span className = "prev-text">Prev</span> 
+          <span class="material-icons md-14 prev-next-icon">navigate_before</span>
+          <span className="prev-text">Prev</span>
         </button>
         <button
-          className="button-nav next focusable" 
-          tabIndex = "0"
+          className="button-nav next focusable"
+          tabIndex="0"
           onClick={(event) => {
             event.preventDefault();
             return this.nextEntry(event);
           }}
         >
-          <span className = "next-text">Next</span> 
-          <span 
-          class="material-icons md-14 prev-next-icon"
-          >
-            navigate_next
-          </span>
+          <span className="next-text">Next</span>
+          <span class="material-icons md-14 prev-next-icon">navigate_next</span>
         </button>
       </div>
     );
 
     let featureCalloutWidget = (
       <div>
-        <div className = "triangle-button-container">
-        <button
-          className="button-info focusable material-icons md-14"
-          tabIndex = "0"
-          onClick={(event) => {
-            event.preventDefault();
-            let widget = document.getElementById("widget");
-            return this.toggleWidget(event, widget);
-          }}
-        >
-          info
-        </button>
-        <div 
-          class="tooltip-triangle"
+        <div className="triangle-button-container">
+          <button
+            className="button-info focusable material-icons md-14"
+            tabIndex="0"
+            onClick={(event) => {
+              event.preventDefault();
+              let widget = document.getElementById("widget");
+              return this.toggleWidget(event, widget);
+            }}
           >
-        </div>
+            info
+          </button>
+          <div class="tooltip-triangle"></div>
         </div>
         <div className="widget-container" id="widget">
           {widgetHeader}
@@ -208,7 +202,6 @@ export default class App extends Component {
       <Router>
         <I18n>
           <ScrollToTop>
-            
             <div className="App__warning old_safari_only">
               <Trans render="h3">
                 Warning! This site doesn't fully work on older versions of Safari. Try a{" "}
@@ -223,7 +216,7 @@ export default class App extends Component {
                 </span>
               )}
               <nav className="inline">
-              {featureCalloutWidget}
+                {featureCalloutWidget}
                 <LocaleNavLink exact to={paths.home}>
                   <Trans>Home</Trans>
                 </LocaleNavLink>
