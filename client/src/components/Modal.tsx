42f3fda6e9996c10bd12263ad4a89e0e78cada0b
Sam Rabiyah <sraby@users.noreply.github.com>
2021-06-24 10:18:09 -0400

    Change modal background color

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -19,8 +19,7 @@ const Modal = (props: ModalProps) => {
       left: 0,
       right: 0,
       bottom: 0,
-      backgroundColor: "rgba(255, 255, 255, 0.75)",
-      //backgroundColor: "rgba(69, 77, 93, 0.6)", // rgba equivalent of the $dark color (#454D5D)
+      backgroundColor: "rgba(69, 77, 93, 0.6)", // rgba equivalent of the $dark color (#454D5D)
     },
     content: {
       position: "absolute",
