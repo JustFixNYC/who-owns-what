f0ecdaf5ada6636e4f7cafef18106bd3c827597b
Atul Varma <varmaa@gmail.com>
2021-06-25 15:08:47 -0400

    Run Prettier.

--- client/src/fullstory.d.ts
+++ client/src/fullstory.d.ts
@@ -1,9 +1,9 @@
 /**
  * Inject the FullStory script and start recording the current user's session.
- * 
+ *
  * This function is idempotent--it only does anything on the first time it's called,
  * and subsequent calls won't do anything harmful.
- * 
+ *
  * This exists so that we can trigger FullStory recording from front-end events
  * instead of recording every single visitor to the site (which causes us to
  * run out of FullStory recording credits very quickly).
