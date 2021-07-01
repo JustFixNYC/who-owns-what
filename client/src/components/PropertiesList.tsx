59af7a4c3719a3ff5fd9c553b7048b4cd928a37c
Sam Rabiyah <sraby@users.noreply.github.com>
2021-07-01 09:05:30 -0400

    Make violations columns slightly wider

--- client/src/components/PropertiesList.tsx
+++ client/src/components/PropertiesList.tsx
@@ -268,7 +268,7 @@ const PropertiesListWithoutI18n: React.FC<
                 ),
                 accessor: (d) => d.openviolations,
                 id: "openviolations",
-                width: getWidthFromLabel(i18n._(t`Open`)),
+                width: getWidthFromLabel(i18n._(t`Open`), 80),
               },
               {
                 Header: (
@@ -279,7 +279,7 @@ const PropertiesListWithoutI18n: React.FC<
                 ),
                 accessor: (d) => d.totalviolations,
                 id: "totalviolations",
-                width: getWidthFromLabel(i18n._(t`Total`)),
+                width: getWidthFromLabel(i18n._(t`Total`), 80),
               },
             ],
           },
