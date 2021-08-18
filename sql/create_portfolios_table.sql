4c4a745b298c4f18dc5ac2bb88bb658a2b0672ba
Atul Varma <varmaa@gmail.com>
2021-08-18 09:45:10 -0400

    Create indexes on bbl and landlord cols.

--- sql/create_portfolios_table.sql
+++ sql/create_portfolios_table.sql
@@ -4,3 +4,5 @@ CREATE TABLE wow_portfolios (
     landlord_names text[],
     graph json
 );
+CREATE INDEX ON wow_portfolios USING GIN(bbls);
+CREATE INDEX ON wow_portfolios USING GIN(landlord_names);
