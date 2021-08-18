dea51c4c2d586dead2589a88da9b065f21b96109
Sam Rabiyah <sraby@users.noreply.github.com>
2021-08-18 16:21:27 -0400

    Refactor AddressToolbar as functional component

--- client/src/components/AddressToolbar.tsx
+++ client/src/components/AddressToolbar.tsx
@@ -5,6 +5,7 @@ import Modal from "./Modal";
 import "styles/AddressToolbar.css";
 import { Trans } from "@lingui/macro";
 import { SearchAddress } from "./AddressSearch";
+import { useState } from "react";
 
 export type AddressToolbarProps = {
   onExportClick: () => void;
@@ -16,66 +17,52 @@ type State = {
   showExportModal: boolean;
 };
 
-export default class AddressToolbar extends Component<AddressToolbarProps, State> {
-  constructor(props: AddressToolbarProps) {
-    super(props);
+const AddressToolbar: React.FC<AddressToolbarProps> = ({
+  onExportClick,
+  numOfAssocAddrs,
+  searchAddr,
+}) => {
+  const [showExportModal, setExportModalVisibility] = useState(false);
+  const userAddrStr = `${searchAddr.housenumber} ${searchAddr.streetname}, ${searchAddr.boro}`;
 
-    this.state = {
-      showExportModal: false,
-    };
-  }
-
-  render() {
-    // fancy default syntax - if userArr is null, bbl keeps ''
-    // let { bbl = '' } = this.props.userAddr;
-    // let boro, block, lot;
-
-    // if (bbl.length) {
-    //   ({ boro, block, lot } = Helpers.splitBBL(bbl));
-    // }
-
-    const userAddrStr = `${this.props.searchAddr.housenumber} ${this.props.searchAddr.streetname}, ${this.props.searchAddr.boro}`;
-
-    return (
-      <div className="AddressToolbar">
-        <div className="btn-group float-right">
-          <Link
-            className="btn btn-primary"
-            onClick={() => {
-              window.gtag("event", "new-search");
-            }}
-            to="/"
-          >
-            <Trans>New Search</Trans>
-          </Link>
-          <button className="btn" onClick={() => this.setState({ showExportModal: true })}>
-            <Trans>Export Data</Trans>
-          </button>
-        </div>
-        <Modal
-          showModal={this.state.showExportModal}
-          onClose={() => this.setState({ showExportModal: false })}
+  return (
+    <div className="AddressToolbar">
+      <div className="btn-group float-right">
+        <Link
+          className="btn btn-primary"
+          onClick={() => {
+            window.gtag("event", "new-search");
+          }}
+          to="/"
         >
-          <Trans render="p">
-            This will export <b>{this.props.numOfAssocAddrs}</b> addresses associated with the
-            landlord at <b>{userAddrStr}</b>!
-          </Trans>
-          <Trans render="p">
-            This data is in <u>CSV file format</u>, which can easily be used in Excel, Google
-            Sheets, or any other spreadsheet program.
-          </Trans>
-          <br />
-          <button
-            className="btn centered"
-            onClick={() => {
-              window.gtag("event", "export-data");
-              this.props.onExportClick();
-            }}
-          >
-            <Trans>Download</Trans>
-          </button>
-        </Modal>
+          <Trans>New Search</Trans>
+        </Link>
+        <button className="btn" onClick={() => setExportModalVisibility(true)}>
+          <Trans>Export Data</Trans>
+        </button>
       </div>
-    );
-  }
-}
+      <Modal showModal={showExportModal} onClose={() => setExportModalVisibility(false)}>
+        <Trans render="p">
+          This will export <b>{numOfAssocAddrs}</b> addresses associated with the landlord at{" "}
+          <b>{userAddrStr}</b>!
+        </Trans>
+        <Trans render="p">
+          This data is in <u>CSV file format</u>, which can easily be used in Excel, Google Sheets,
+          or any other spreadsheet program.
+        </Trans>
+        <br />
+        <button
+          className="btn centered"
+          onClick={() => {
+            window.gtag("event", "export-data");
+            onExportClick();
+          }}
+        >
+          <Trans>Download</Trans>
+        </button>
+      </Modal>
+    </div>
+  );
+};
+
+export default AddressToolbar;
