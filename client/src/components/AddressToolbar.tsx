b9a7ad1b0b99c5645b47a9d75cad18f593e7886c
Sam Rabiyah <sraby@users.noreply.github.com>
2021-08-18 16:56:00 -0400

    Finish adding toggle button to addresstoolbar

--- client/src/components/AddressToolbar.tsx
+++ client/src/components/AddressToolbar.tsx
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
-import { LocaleLink as Link } from "../i18n";
+import React from "react";
+import { LocaleLink as Link, parseLocaleFromPath, removeLocalePrefix } from "../i18n";
 import Modal from "./Modal";
 
 import "styles/AddressToolbar.css";
 import { Trans } from "@lingui/macro";
 import { SearchAddress } from "./AddressSearch";
 import { useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
 export type AddressToolbarProps = {
   onExportClick: () => void;
@@ -13,8 +14,25 @@ export type AddressToolbarProps = {
   searchAddr: SearchAddress;
 };
 
-type State = {
-  showExportModal: boolean;
+const ToggleButtonBetweenPortfolioMethods = () => {
+  const history = useHistory();
+  const { pathname } = useLocation();
+  const isWowzaRoute = removeLocalePrefix(pathname).startsWith("/wowza");
+  const toggleUrlChange = () => {
+    if (isWowzaRoute) {
+      history.push(pathname.replace("/wowza", ""));
+    } else {
+      const locale = parseLocaleFromPath(pathname);
+      const newUrl = `/${locale}/wowza${removeLocalePrefix(pathname)}`;
+      history.push(newUrl);
+    }
+    history.go(0);
+  };
+  return (
+    <button className="btn btn-justfix" onClick={() => toggleUrlChange()}>
+      {isWowzaRoute ? <Trans>Switch to Old Version</Trans> : <Trans>Switch to New Version</Trans>}
+    </button>
+  );
 };
 
 const AddressToolbar: React.FC<AddressToolbarProps> = ({
@@ -22,12 +40,15 @@ const AddressToolbar: React.FC<AddressToolbarProps> = ({
   numOfAssocAddrs,
   searchAddr,
 }) => {
+  const allowChangingPortfolioMethod = process.env.REACT_APP_ENABLE_NEW_WOWZA_PORTFOLIO_MAPPING;
   const [showExportModal, setExportModalVisibility] = useState(false);
+
   const userAddrStr = `${searchAddr.housenumber} ${searchAddr.streetname}, ${searchAddr.boro}`;
 
   return (
     <div className="AddressToolbar">
       <div className="btn-group float-right">
+        {allowChangingPortfolioMethod && <ToggleButtonBetweenPortfolioMethods />}
         <Link
           className="btn btn-primary"
           onClick={() => {
