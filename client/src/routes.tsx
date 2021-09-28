5472aa3a5c43a603ce8efd9093f9d7f3b8505508
Sam Rabiyah <sraby@users.noreply.github.com>
2021-09-28 16:22:46 -0400

    Allow "wowza" prefix within bbl-based URLs

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -58,16 +58,20 @@ export const createWhoOwnsWhatRoutePaths = (prefix?: string) => {
     ),
     /** Note: this path doesn't correspond to a stable page on the site. It simply provides an entry point that
      * immediately redirects to an addressPageOverview. This path is helpful for folks who, say, have a list of
-     * boro, block, lot values in a spreadsheet and want to easily generate direct links to WhoOwnsWhat.
+     * bbl values in a spreadsheet and want to easily generate direct links to WhoOwnsWhat.
      * See `BBLPage.tsx` for more details.
      */
-    bbl: `${pathPrefix}/bbl/:boro/:block/:lot`,
+    bbl: `${pathPrefix}/bbl/:bbl`,
     /** Note: this path doesn't correspond to a stable page on the site. It simply provides an entry point that
      * immediately redirects to an addressPageOverview. This path is helpful for folks who, say, have a list of
      * bbl values in a spreadsheet and want to easily generate direct links to WhoOwnsWhat.
      * See `BBLPage.tsx` for more details.
      */
-    bblWithFullBblInUrl: `${pathPrefix}/bbl/:bbl`,
+    wowzaBbl: `${pathPrefix}/wowza/bbl/:bbl`,
+    /** This route path corresponds to a page identical to the `bbl` route above, but with an older url
+     * pattern that we want to support so as not to break any old links that exist out in the web.
+     */
+    oldBbl: `${pathPrefix}/bbl/:boro/:block/:lot`,
     about: `${pathPrefix}/about`,
     howToUse: `${pathPrefix}/how-to-use`,
     methodology: `${pathPrefix}/how-it-works`,
