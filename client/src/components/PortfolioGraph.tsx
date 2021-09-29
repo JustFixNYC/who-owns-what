baab072adc5333a65f45804ebf64ab456c8d59ea
Samara Trilling <samara.trilling@gmail.com>
2021-09-29 15:37:34 -0400

    Show searchBBL and addrBBL to graph

--- client/src/components/PortfolioGraph.tsx
+++ client/src/components/PortfolioGraph.tsx
@@ -3,29 +3,28 @@ import Cytoscape from "cytoscape";
 import CytoscapeComponent from "react-cytoscapejs";
 // @ts-ignore
 import fcose from "cytoscape-fcose";
-import { RawPortfolioGraphJson } from "./APIDataTypes";
+import { AddressRecord, RawPortfolioGraphJson } from "./APIDataTypes";
+import { withMachineInStateProps } from "state-machine";
+import helpers from "util/helpers";
 
 Cytoscape.use(fcose);
 
-const formatGraphJSON = (rawJSON: RawPortfolioGraphJson): cytoscape.ElementDefinition[] => {
-  const nodes: cytoscape.ElementDefinition[] = rawJSON.nodes.map((node) => ({
-    group: "nodes",
-    data: {
-      id: node.id.toString(),
-      type: node.value.kind,
-      value: node.value.value,
-    },
-  }));
+const formatGraphJSON = (
+  rawJSON: RawPortfolioGraphJson,
+  additionalNodes: Cytoscape.NodeDefinition[],
+  additionalEdges: Cytoscape.EdgeDefinition[]
+): cytoscape.ElementDefinition[] => {
+  let nodes: cytoscape.ElementDefinition[] = rawJSON.nodes.map((node) => createNode(
+    node.id.toString(),
+    node.value.kind,
+    node.value.value)
+  );
+  nodes = nodes.concat(additionalNodes);
 
-  const edges: cytoscape.ElementDefinition[] = rawJSON.edges.map((edge) => ({
-    group: "edges",
-    data: {
-      source: edge.from.toString(),
-      target: edge.to.toString(),
-      count_hpd_registrations: edge.reg_contacts,
-    },
-  }));
-
+  const edges: cytoscape.ElementDefinition[] = rawJSON.edges.map((edge) =>
+    createEdge(edge.from.toString(), edge.to.toString(), edge.reg_contacts)
+  );
+  nodes = edges.concat(additionalEdges);
   return nodes.concat(edges);
 };
 
@@ -38,20 +37,113 @@ const layout = {
   nodeSeparation: 300,
 };
 
-export const PortfolioGraph: React.FC<{ graphJSON: RawPortfolioGraphJson }> = ({ graphJSON }) => (
-  <CytoscapeComponent
-    elements={formatGraphJSON(graphJSON)}
-    style={{ width: "900px", height: "600px" }}
-    layout={layout}
-    stylesheet={[
-      {
-        selector: "node",
-        style: {
-          label: "data(value)",
-          backgroundColor: (ele) => (ele.data("type") === "name" ? "red" : "green"),
-          "min-zoomed-font-size": 16,
+type PortfolioGraphProps = withMachineInStateProps<"portfolioFound"> & {
+  graphJSON: RawPortfolioGraphJson;
+};
+
+function createNode(id: string, type: string, value: string): cytoscape.NodeDefinition {
+  return {
+    group: "nodes",
+    data: {
+      id,
+      type,
+      value,
+    },
+  };
+}
+
+function createEdge(
+  source: string,
+  target: string,
+  count_hpd_registrations: number=1
+): cytoscape.EdgeDefinition {
+  return {
+    group: "edges",
+    data: {
+      source,
+      target,
+      count_hpd_registrations,
+    },
+  };
+}
+
+/**
+ * Generates one node for the search BBL and one for the detail BBL to show on the portfolio graph.
+ * @param searchAddr 
+ * @param detailAddr 
+ * @returns 
+ */
+function generateAdditionalNodes(searchAddr: AddressRecord, detailAddr?: AddressRecord) {
+  var additionalNodes = [];
+  const searchBBLNode = createNode(
+    "searchaddr",
+    "searchaddr",
+    `${searchAddr.housenumber} ${searchAddr.streetname}, ${searchAddr.boro}`
+  );
+  additionalNodes.push(searchBBLNode);
+
+  if (detailAddr) {
+    const detailBBLNode = createNode(
+      "detailaddr",
+      "detailaddr",
+      `${detailAddr.housenumber} ${detailAddr.streetname}, ${detailAddr.boro}`
+    );
+    additionalNodes.push(detailAddr);
+  }
+  return additionalNodes;
+}
+
+function getLandlordNodeIDFromAddress(addr: AddressRecord, landlordNodes: RawPortfolioGraphJson) {
+  const ll_name = helpers.getLandlordNameFromAddress(addr);
+  // TODO: look through landlordNodes to find the one that matches LL_name, return that 
+}
+
+function generateAdditionalEdges(searchAddr: AddressRecord, detailAddr?: AddressRecord) {
+  var additionalEdges = [];
+  const searchBBLEdge = createEdge(
+    getLandlordNodeIDFromAddress(searchAddr),
+    'searchAddr'
+  );
+  additionalEdges.push(searchBBLEdge);
+  if (detailAddr) {
+    const detailBBLEdge = createEdge(
+      getLandlordNodeIDFromAddress(detailAddr),
+      'detailaddr' 
+    );
+    additionalEdges.push(detailBBLEdge);
+  }
+  return additionalEdges;
+}
+
+
+
+export const PortfolioGraph: React.FC<PortfolioGraphProps> = ({ graphJSON, state }) => {
+  const { searchAddr, detailAddr } = state.context.portfolioData;
+  const additionalNodes = generateAdditionalNodes(searchAddr, detailAddr);
+  const additionalEdges = generateAdditionalEdges(searchAddr, detailAddr);
+
+  return (
+    <CytoscapeComponent
+      elements={formatGraphJSON(graphJSON, additionalNodes, additionalEdges)}
+      style={{ width: "900px", height: "600px" }}
+      layout={layout}
+      stylesheet={[
+        {
+          selector: "node",
+          style: {
+            label: "data(value)",
+            backgroundColor: (ele) => (NODE_TYPE_TO_COLOR[ele.data("type")]),
+            "min-zoomed-font-size": 16,
+          },
         },
-      },
-    ]}
-  />
-);
+      ]}
+    />
+  );
+};
+
+const NODE_TYPE_TO_COLOR:Record<string,string> = {
+  'name': 'red',
+  'bizaddr': 'green',
+  'searchaddr': 'orange',
+  'detailaddr': 'yellow',
+};
