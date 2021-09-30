7de9154d9b796505014730bb9d6210f57bf66394
Sam Rabiyah <sraby@users.noreply.github.com>
2021-09-30 15:23:08 -0400

    Add search address and detail address nodes to portfolio graph visualization (#547)
    This PR refactors our <PortfolioGraph> component to dynamically add on nodes to the graph visualization for the user's Search Address, as well as the Detail Address if it differs from the Search Address. The idea behind these additional nodes was to create some feature parity with the "How is this building associated..." feature on the old WOW:
    
    Now, when users click on this link, they are redirected to the Portfolio Graph (currently on the Summary tab) to see how their search address is related to the address in focus on the Overview tab:
    
    This is particularly important because in order to build trust with our users regarding this new portfolio method, we need to have some feature that illustrates how each building in the portfolio connects back to the original address they searched.

--- client/src/components/PortfolioGraph.tsx
+++ client/src/components/PortfolioGraph.tsx
@@ -3,31 +3,11 @@ import Cytoscape from "cytoscape";
 import CytoscapeComponent from "react-cytoscapejs";
 // @ts-ignore
 import fcose from "cytoscape-fcose";
-import { RawPortfolioGraphJson } from "./APIDataTypes";
+import { AddressRecord, PortfolioGraphNode, RawPortfolioGraphJson } from "./APIDataTypes";
+import { withMachineInStateProps } from "state-machine";
+import helpers from "util/helpers";
 
 Cytoscape.use(fcose);
-
-const formatGraphJSON = (rawJSON: RawPortfolioGraphJson): cytoscape.ElementDefinition[] => {
-  const nodes: cytoscape.ElementDefinition[] = rawJSON.nodes.map((node) => ({
-    group: "nodes",
-    data: {
-      id: node.id.toString(),
-      type: node.value.kind,
-      value: node.value.value,
-    },
-  }));
-
-  const edges: cytoscape.ElementDefinition[] = rawJSON.edges.map((edge) => ({
-    group: "edges",
-    data: {
-      source: edge.from.toString(),
-      target: edge.to.toString(),
-      count_hpd_registrations: edge.reg_contacts,
-    },
-  }));
-
-  return nodes.concat(edges);
-};
 
 const layout = {
   name: "fcose",
@@ -38,20 +18,148 @@ const layout = {
   nodeSeparation: 300,
 };
 
-export const PortfolioGraph: React.FC<{ graphJSON: RawPortfolioGraphJson }> = ({ graphJSON }) => (
-  <CytoscapeComponent
-    elements={formatGraphJSON(graphJSON)}
-    style={{ width: "900px", height: "600px" }}
-    layout={layout}
-    stylesheet={[
-      {
-        selector: "node",
-        style: {
-          label: "data(value)",
-          backgroundColor: (ele) => (ele.data("type") === "name" ? "red" : "green"),
-          "min-zoomed-font-size": 16,
+function createNode(id: string, type: string, value: string): cytoscape.NodeDefinition {
+  return {
+    group: "nodes",
+    data: {
+      id,
+      type,
+      value,
+    },
+  };
+}
+
+function createEdge(
+  source: string,
+  target: string,
+  count_hpd_registrations: number = 1
+): cytoscape.EdgeDefinition {
+  return {
+    group: "edges",
+    data: {
+      source,
+      target,
+      count_hpd_registrations,
+    },
+  };
+}
+
+/**
+ * Generates one node for the search BBL and one for the detail BBL to show on the portfolio graph.
+ */
+function generateAdditionalNodes(searchAddr: AddressRecord, detailAddr?: AddressRecord) {
+  var additionalNodes = [];
+  const searchBBLNode = createNode(
+    "searchaddr",
+    "searchaddr",
+    `${searchAddr.housenumber} ${searchAddr.streetname}, ${searchAddr.boro}`
+  );
+  additionalNodes.push(searchBBLNode);
+
+  if (detailAddr) {
+    const detailBBLNode = createNode(
+      "detailaddr",
+      "detailaddr",
+      `${detailAddr.housenumber} ${detailAddr.streetname}, ${detailAddr.boro}`
+    );
+    additionalNodes.push(detailBBLNode);
+  }
+  return additionalNodes;
+}
+
+/**
+ * Given an existing array of graph nodes and an Address Record, this function
+ * finds the `id` property of a node of type `name` that corresponds to the owner
+ * (or owners) listed with the address.
+ */
+export function getLandlordNodeIDFromAddress(
+  existingNodes: PortfolioGraphNode[],
+  addr: AddressRecord
+) {
+  const landlordNames = helpers.getLandlordNameFromAddress(addr);
+  const landlordMatches = existingNodes.filter(
+    (node) => node.value.kind === "name" && landlordNames.includes(node.value.value)
+  );
+  return landlordMatches.map((node) => node.id.toString());
+}
+
+/**
+ * Given the existing array of graph nodes, this function generates an edge from search BBL to its corresponding owner name,
+ * as well as an edge from the detail BBL to its corresponding owner name.
+ *
+ * Note: in rare edge cases, there may be more than one corresponding owner name for a given BBL.
+ * In these cases, multiple edges may be created here.
+ */
+function generateAdditionalEdges(
+  existingNodes: PortfolioGraphNode[],
+  searchAddr: AddressRecord,
+  detailAddr?: AddressRecord
+) {
+  let additionalEdges: Cytoscape.EdgeDefinition[] = [];
+  const searchBBLEdges = getLandlordNodeIDFromAddress(existingNodes, searchAddr).map((id) =>
+    createEdge(id, "searchaddr")
+  );
+  additionalEdges = additionalEdges.concat(searchBBLEdges);
+  if (detailAddr) {
+    const detailBBLEdges = getLandlordNodeIDFromAddress(existingNodes, detailAddr).map((id) =>
+      createEdge(id, "detailaddr")
+    );
+    additionalEdges = additionalEdges.concat(detailBBLEdges);
+  }
+  return additionalEdges;
+}
+
+const formatGraphJSON = (
+  rawJSON: RawPortfolioGraphJson,
+  additionalNodes: Cytoscape.NodeDefinition[],
+  additionalEdges: Cytoscape.EdgeDefinition[]
+): cytoscape.ElementDefinition[] => {
+  let nodes: cytoscape.ElementDefinition[] = rawJSON.nodes.map((node) =>
+    createNode(node.id.toString(), node.value.kind, node.value.value)
+  );
+  nodes = nodes.concat(additionalNodes);
+
+  let edges: cytoscape.ElementDefinition[] = rawJSON.edges.map((edge) =>
+    createEdge(edge.from.toString(), edge.to.toString(), edge.reg_contacts)
+  );
+  edges = edges.concat(additionalEdges);
+  return nodes.concat(edges);
+};
+
+type PortfolioGraphProps = Pick<withMachineInStateProps<"portfolioFound">, "state"> & {
+  graphJSON: RawPortfolioGraphJson;
+};
+
+export const PortfolioGraph: React.FC<PortfolioGraphProps> = ({ graphJSON, state }) => {
+  const { searchAddr, detailAddr } = state.context.portfolioData;
+  const distinctDetailAddr = !helpers.addrsAreEqual(searchAddr, detailAddr)
+    ? detailAddr
+    : undefined;
+  const additionalNodes = generateAdditionalNodes(searchAddr, distinctDetailAddr);
+  const additionalEdges = generateAdditionalEdges(graphJSON.nodes, searchAddr, distinctDetailAddr);
+
+  return (
+    <CytoscapeComponent
+      elements={formatGraphJSON(graphJSON, additionalNodes, additionalEdges)}
+      style={{ width: "900px", height: "600px" }}
+      layout={layout}
+      stylesheet={[
+        {
+          selector: "node",
+          style: {
+            label: "data(value)",
+            backgroundColor: (ele) => NODE_TYPE_TO_COLOR[ele.data("type")],
+            "min-zoomed-font-size": 16,
+          },
         },
-      },
-    ]}
-  />
-);
+      ]}
+    />
+  );
+};
+
+const NODE_TYPE_TO_COLOR: Record<string, string> = {
+  name: "red",
+  bizaddr: "green",
+  searchaddr: "orange",
+  detailaddr: "yellow",
+};
