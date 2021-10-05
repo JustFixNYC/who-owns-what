26eb5196fe5677d8c77286d128c42e5f003953fb
Sam Rabiyah <sraby@users.noreply.github.com>
2021-10-05 18:25:48 -0400

    Add testing infrastructure

---
+++ tests/factories/dof_exemption_classification_codes.py
@@ -0,0 +1,11 @@
+from typing import NamedTuple
+
+
+class DofExemptionClassificationCodes(NamedTuple):
+    exemptcode: str = '1010'
+    sdeacode: str = '41101'
+    description: str = 'VETERAN'
+    status: str = 'ACTIVE'
+    legalref: str = 'RPTL § 458'
+    comments: str = ''
+    updated: str = '2013-11-25'
