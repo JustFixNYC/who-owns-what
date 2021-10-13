448e97cdde4f67df6c55a099a81aced84629b885
Sam Rabiyah <sraby@users.noreply.github.com>
2021-10-13 17:36:39 -0400

    flake8

--- portfoliograph/hpd_regs.py
+++ portfoliograph/hpd_regs.py
@@ -8,11 +8,9 @@ def build_reg_bbl_map(dict_cursor) -> RegBblMap:
     reg_bbl_map: RegBblMap = {}
     dict_cursor.execute(
         f"""
-        SELECT 
-            registrationid,
-	        first(bbl) bbl
+        SELECT registrationid, first(bbl) bbl
         FROM (
-            SELECT * FROM hpd_registrations 
+            SELECT * FROM hpd_registrations
             ORDER BY registrationenddate DESC
         ) REGS
         GROUP BY registrationid
