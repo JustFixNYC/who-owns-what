7b5d76515f45999632af1b39a4aa914ef71e246c
Sam Rabiyah <sraby@users.noreply.github.com>
2022-01-19 11:29:59 -0500

    portfolio graph tweaks

--- client/src/components/PortfolioGraph.tsx
+++ client/src/components/PortfolioGraph.tsx
@@ -14,7 +14,7 @@ const layout = {
   nodeDimensionsIncludeLabels: true,
   animate: false,
   quality: "proof",
-  idealEdgeLength: 200,
+  idealEdgeLength: 100,
   nodeSeparation: 300,
 };
 
@@ -52,7 +52,7 @@ function generateAdditionalNodes(searchAddr: AddressRecord, detailAddr?: Address
   const searchBBLNode = createNode(
     "searchaddr",
     "searchaddr",
-    `${searchAddr.housenumber} ${searchAddr.streetname}, ${searchAddr.boro}`
+    `SEARCH ADDRESS: ${searchAddr.housenumber} ${searchAddr.streetname}`
   );
   additionalNodes.push(searchBBLNode);
 
@@ -60,7 +60,7 @@ function generateAdditionalNodes(searchAddr: AddressRecord, detailAddr?: Address
     const detailBBLNode = createNode(
       "detailaddr",
       "detailaddr",
-      `${detailAddr.housenumber} ${detailAddr.streetname}, ${detailAddr.boro}`
+      `SELECTED ADDRESS: ${detailAddr.housenumber} ${detailAddr.streetname}`
     );
     additionalNodes.push(detailBBLNode);
   }
@@ -141,15 +141,35 @@ export const PortfolioGraph: React.FC<PortfolioGraphProps> = ({ graphJSON, state
   return (
     <CytoscapeComponent
       elements={formatGraphJSON(graphJSON, additionalNodes, additionalEdges)}
-      style={{ width: "900px", height: "600px" }}
+      style={{ width: "100%", height: "60vh" }}
       layout={layout}
       stylesheet={[
         {
           selector: "node",
           style: {
             label: "data(value)",
+            width: (ele: Cytoscape.NodeSingular) => (ele.data("type") === "bizaddr" ? 20 : 30),
+            height: (ele: Cytoscape.NodeSingular) => (ele.data("type") === "bizaddr" ? 20 : 30),
+            "text-wrap": "wrap",
+            "text-max-width": "200px",
+            "font-size": (ele: Cytoscape.NodeSingular) =>
+              ele.data("type") === "bizaddr" ? "12px" : "15px",
+            "font-weight": (ele: Cytoscape.NodeSingular) =>
+              ["searchaddr", "detailaddr"].includes(ele.data("type")) ? 700 : 400,
+            "font-family": "Inconsolata, monospace",
             backgroundColor: (ele) => NODE_TYPE_TO_COLOR[ele.data("type")],
             "min-zoomed-font-size": 16,
+          },
+        },
+        {
+          selector: "edge",
+          style: {
+            "line-color": (ele: Cytoscape.EdgeSingular) =>
+              ele.data("target") === "searchaddr"
+                ? "orange"
+                : ele.data("target") === "detailaddr"
+                ? "yellow"
+                : "default",
           },
         },
       ]}
@@ -159,7 +179,7 @@ export const PortfolioGraph: React.FC<PortfolioGraphProps> = ({ graphJSON, state
 
 const NODE_TYPE_TO_COLOR: Record<string, string> = {
   name: "red",
-  bizaddr: "green",
+  bizaddr: "gray",
   searchaddr: "orange",
   detailaddr: "yellow",
 };
