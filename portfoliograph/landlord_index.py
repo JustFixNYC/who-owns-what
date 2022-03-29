44646509d3ba23d16f442a05db39a1e8fa813d29
Samara Trilling <samara.trilling@gmail.com>
2022-03-29 12:39:44 -0400

    Finish skeleton of how to update algolia index

--- portfoliograph/landlord_index.py
+++ portfoliograph/landlord_index.py
@@ -1,5 +1,6 @@
 def update_landlord_algolia_index(conn):
     # TODO: Check whether wow_portfolios table has changed
+
     cleaned_rows = []
     with conn.cursor() as cursor:
         cursor.execute(
@@ -12,29 +13,57 @@ def update_landlord_algolia_index(conn):
             WHERE bbls[1] is not null;
         """
         )
+
+    # 3. Check if stringified ll_names is longer than 2000 characters
     for row in cursor.fetchall():
-        if len(row["landlord_names"]) > 2000:
+        # Avoid Algolia's limit on article size by breaking up the landlord
+        # names list into multiple records that each point to the same BBLs.
+        num_parts = len(row["landlord_names"]) // 2000
+        if num_parts > 1:
             names_array = row["landlord_names"].split(', ')
-            first_half = names_array[:len(names_array)//2]
-            second_half = names_array[len(names_array)//2:]
-            first_half_string = ', '.join(first_half)
-            second_half_string = ', '.join(second_half)
-            new_row_1 = {"bbl": row["bbl"], "landlord_names": first_half_string}
-            new_row_2 = {"bbl": row["bbl"], "landlord_names": second_half_string}
-            cleaned_rows.append(new_row_1).append(new_row_2)
+            parts = numpy.array_split(names_array, num_parts)
+            new_rows = [{"bbl": row["bbl"], "landlord_names": ', '.join(part)} for part in parts]
+            cleaned_rows.extend(new_rows)
         else:
             cleaned_rows.append(row)
 
-    # 3. Check if stringifued ll_names is longer than 2000 character 
-    # 4. Upload CSV to Algolio as new index
+    # 4. Upload CSV to Algolia as new index
+    from algoliasearch.search_client import SearchClient
+    from dotenv import load_dotenv, find_dotenv
+    load_dotenv(find_dotenv())
 
-    outfile.write("[\n")
-    components_written = 0
+    # Algolia client credentials
+    ALGOLIA_APP_ID = getenv('ALGOLIA_APP_ID')
+    ALGOLIA_API_KEY = getenv('ALGOLIA_API_KEY')
+    ALGOLIA_INDEX_NAME = getenv('ALGOLIA_INDEX_NAME')
 
-    for pr in iter_portfolio_rows(conn):
-        if components_written > 0:
-            outfile.write(",\n")
-        outfile.write(json.dumps(pr.to_json()))
-        components_written += 1
+    # Initialize the client
+    # https://www.algolia.com/doc/api-client/getting-started/instantiate-client-index/?client=python
+    client = SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
 
-    outfile.write("]\n")
+    # Initialize an index
+    # https://www.algolia.com/doc/api-client/getting-started/instantiate-client-index/#initialize-an-index
+    index = client.init_index(ALGOLIA_INDEX_NAME)
+    """
+    Of shape:
+
+    new_contacts = [
+    {
+        'name': 'NewFoo',
+        'objectID': '3'
+    },
+    {
+        'name': 'NewBar',
+        'objectID': '4'
+    }
+    ]
+    """
+    # Replace All Objects: Clears all objects from your index and replaces them with a new set of objects.
+    # https://www.algolia.com/doc/api-reference/api-methods/replace-all-objects/?client=python  
+    index.replace_all_objects(cleaned_rows).wait()
+
+    """
+    To test:
+        res = index.search('')
+        print('Current objects: ', res['hits'], '\n')
+    """
