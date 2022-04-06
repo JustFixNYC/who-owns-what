8b1f8b0668a881c62b6f16839ba6722a273fa501
Sam Rabiyah <sraby@users.noreply.github.com>
2022-04-06 16:18:48 -0400

    fix tests

--- client/src/routes.test.tsx
+++ client/src/routes.test.tsx
@@ -12,7 +12,7 @@ describe("createAddressPageRoutes()", () => {
         housenumber: "654",
         streetname: "PARK PLACE",
       }).timeline
-    ).toBe("/address/BROOKLYN/654/PARK%20PLACE/timeline");
+    ).toBe("/legacy/address/BROOKLYN/654/PARK%20PLACE/timeline");
   });
 
   it("prefixes with address page params and locale when given one", () => {
@@ -23,6 +23,20 @@ describe("createAddressPageRoutes()", () => {
         streetname: "PARK PLACE",
         locale: "es",
       }).timeline
+    ).toBe("/es/legacy/address/BROOKLYN/654/PARK%20PLACE/timeline");
+  });
+
+  it("correctly sets the right path when route is specified as a wowza route", () => {
+    expect(
+      createAddressPageRoutes(
+        {
+          boro: "BROOKLYN",
+          housenumber: "654",
+          streetname: "PARK PLACE",
+          locale: "es",
+        },
+        true
+      ).timeline
     ).toBe("/es/address/BROOKLYN/654/PARK%20PLACE/timeline");
   });
 });
