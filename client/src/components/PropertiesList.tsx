62a08f9f019fc5e230dde8aec6268f1d25cefb10
Sam Rabiyah <sraby@users.noreply.github.com>
2022-04-06 12:19:07 -0400

    Add JointOwner names to "Owners" column on Portfolio tab

--- client/src/components/PropertiesList.tsx
+++ client/src/components/PropertiesList.tsx
@@ -439,7 +439,10 @@ const TableOfData = React.memo(
                   var owner =
                     d.ownernames &&
                     d.ownernames.find(
-                      (o) => o.title === "HeadOfficer" || o.title === "IndividualOwner"
+                      (o) =>
+                        o.title === "HeadOfficer" ||
+                        o.title === "IndividualOwner" ||
+                        o.title === "JointOwner"
                     );
                   return owner ? owner.value : "";
                 },
