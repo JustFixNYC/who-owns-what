a9b1cb74b04d765c22f08cab9ef36e18e17b81f9
Sam Rabiyah <sraby@users.noreply.github.com>
2022-04-06 16:46:33 -0400

    Fix tests

--- client/src/components/WowzaToggle.test.tsx
+++ client/src/components/WowzaToggle.test.tsx
@@ -2,16 +2,16 @@ import { getPathForOtherPortfolioMethod, isWowzaPath } from "./WowzaToggle";
 
 describe("isWowzaPath()", () => {
   it("works", () => {
-    expect(isWowzaPath("/boop")).toBe(false);
-    expect(isWowzaPath("/wowza")).toBe(false);
-    expect(isWowzaPath("/en/address/1425/wowza/avenue")).toBe(false);
-    expect(isWowzaPath("/en/wowza/blah?bee=1")).toBe(true);
+    expect(isWowzaPath("/boop")).toBe(true);
+    expect(isWowzaPath("/wowza")).toBe(true);
+    expect(isWowzaPath("/en/address/1425/wowza/avenue")).toBe(true);
+    expect(isWowzaPath("/en/legacy/blah?bee=1")).toBe(false);
   });
 });
 
 describe("getPathForOtherPortfolioMethod()", () => {
   it("works", () => {
-    expect(getPathForOtherPortfolioMethod("/en/address/boop")).toBe("/en/wowza/address/boop");
-    expect(getPathForOtherPortfolioMethod("/es/wowza/address/blah")).toBe("/es/address/blah");
+    expect(getPathForOtherPortfolioMethod("/en/address/boop")).toBe("/en/legacy/address/boop");
+    expect(getPathForOtherPortfolioMethod("/es/legacy/address/blah")).toBe("/es/address/blah");
   });
 });
