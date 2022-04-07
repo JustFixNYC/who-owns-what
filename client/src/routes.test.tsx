721382fd9eb17f70ed7c58db86e55dc3aafb6724
Sam Rabiyah <sraby@users.noreply.github.com>
2022-04-07 15:30:33 -0400

    Attempt to parametrize env in test

--- client/src/routes.test.tsx
+++ client/src/routes.test.tsx
@@ -1,6 +1,18 @@
 import { createAddressPageRoutes } from "routes";
 
 describe("createAddressPageRoutes()", () => {
+  const OLD_ENV = process.env;
+
+  // https://stackoverflow.com/questions/48033841/test-process-env-with-jest
+  beforeEach(() => {
+    jest.resetModules(); // Most important - it clears the cache
+    process.env = { ...OLD_ENV }; // Make a copy
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV; // Restore old environment
+  });
+
   it("prefixes with string when given one", () => {
     expect(createAddressPageRoutes("/boop").timeline).toBe("/boop/timeline");
   });
@@ -27,6 +39,7 @@ describe("createAddressPageRoutes()", () => {
   });
 
   it("correctly sets the right path when route is specified as a legacy route", () => {
+    process.env.REACT_APP_ENABLE_NEW_WOWZA_PORTFOLIO_MAPPING = "1";
     expect(
       createAddressPageRoutes(
         {
