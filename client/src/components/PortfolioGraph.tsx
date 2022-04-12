be9154e987ced118075246f9b3e16dc777e8a7a7
Maxwell Austensen <maxwell.austensen@nyu.edu>
2022-04-12 12:50:55 -0400

    prettier

--- client/src/components/PortfolioGraph.tsx
+++ client/src/components/PortfolioGraph.tsx
@@ -244,7 +244,9 @@ const PortfolioGraphWithoutI18: React.FC<PortfolioGraphProps> = ({ graphJSON, st
           <img src={warning} className="icon" alt="Warning" />
           <span className="warning">
             {/* eslint-disable-next-line */}
-            <Trans>This diagram may show a network of portfolios. <button>Learn more.</button></Trans>
+            <Trans>
+              This diagram may show a network of portfolios. <button>Learn more.</button>
+            </Trans>
           </span>
         </div>
       </div>
