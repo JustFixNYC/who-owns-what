4227d55ecbef5034868acedbab9a3409cf05bd0b
Sam Rabiyah <sraby@users.noreply.github.com>
2022-04-20 10:16:33 -0400

    Optimize the run time of the new WOWZA search query (#629)
    I had a feeling that our new WOWZA search function was taking a while to run, often 2-4 seconds per search. While this isn't horrible, I did want to take a second to investigate any ways to cut down on run time.
    
    I realized that the INNER JOIN WOW_BLDGS USING(BBL); line from the search function in address_portfolio.sql was the costliest line of code, so I rewrote the function without a costly join at all. There may be other room for improvement, but this fix cut down the run time to 1/4th of what it was before.

--- wow/sql/address_portfolio.sql
+++ wow/sql/address_portfolio.sql
@@ -1,12 +1,23 @@
+-- Grab the graph json object that corresponds 
+-- to the portfolio of the search address: 
+WITH PORTFOLIO_GRAPH AS (
+	SELECT 
+		%(bbl)s as BBL,
+		GRAPH
+	FROM WOW_PORTFOLIOS
+	WHERE %(bbl)s = ANY(BBLS)
+	LIMIT 1
+)
+
+-- Select a subset of WOW_BLDGS that only contains
+-- BBLs within the same portfolio
 SELECT 
 	WOW_BLDGS.*,
-	CASE 
-		WHEN BBL = %(bbl)s THEN GRAPH
-		ELSE NULL
-	END AS GRAPH
-	FROM (
-		SELECT UNNEST(BBLS) BBL, GRAPH
-		FROM WOW_PORTFOLIOS
-		WHERE %(bbl)s = ANY(BBLS)
-	) T
-INNER JOIN WOW_BLDGS USING(BBL);
+	GRAPH
+FROM WOW_BLDGS
+LEFT JOIN PORTFOLIO_GRAPH USING(BBL)
+WHERE BBL = ANY(
+	SELECT UNNEST(BBLS)
+	FROM WOW_PORTFOLIOS
+	WHERE %(bbl)s = ANY(BBLS)
+);
