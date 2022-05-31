49cc2838b72fa46866948fb4429f570736b345e1
Sam Rabiyah <sraby@users.noreply.github.com>
2022-05-31 16:12:13 -0400

    Add sample wow_portfolios table to test data

--- tests/exported_test_data.sql
+++ tests/exported_test_data.sql
@@ -256,4 +256,12 @@ INSERT INTO public.wow_bldgs (housenumber, streetname, zip, boro, registrationid
 -- PostgreSQL database dump complete
 --
 
+--
+-- Manual creation of sample wow_portfolios table for testing:
+--
 
+INSERT INTO public.wow_portfolios (bbls, landlord_names, graph) VALUES ('{3016780054}','{"DAVID SIMKOWITZ"}','{"nodes": [{"id": 1, "value": {"kind": "bizaddr", "value": "206 JOHNSON AVENUE, STATEN ISLAND NY"}}, {"id": 2, "value": {"kind": "name", "value": "ANTHONY DEMEO"}}], "edges": [{"from": 1, "to": 2, "reg_contacts": 1}]}');
+
+--
+-- Manual table creation complete
+--
