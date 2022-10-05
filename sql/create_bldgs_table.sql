01f01af38868280e752c8077532bd580d92bcda1
Maxwell Austensen <m.austensen@gmail.com>
2022-10-05 11:36:18 -0400

    remove oca schema prefix from sql queries, will instead edit search_path on db

--- sql/create_bldgs_table.sql
+++ sql/create_bldgs_table.sql
@@ -89,7 +89,7 @@ select distinct on (registrations.bbl)
   firstdeeds.docamount as lastsaleamount,
   case
     when coalesce(pluto.unitsres, 0) < 11 then NULL
-    else coalesce(oca.eviction_filings_since_2017, 0) 
+    else coalesce(eviction_filings_since_2017, 0) 
   end as evictionfilings
 from hpd_registrations_with_contacts as registrations
 left join (
@@ -129,7 +129,7 @@ left join (
 left join rentstab on (registrations.bbl = rentstab.ucbbl)
 left join complaints on (registrations.bbl = complaints.bbl)
 left join firstdeeds on (registrations.bbl = firstdeeds.bbl)
-left join oca.oca_evictions_bldgs as oca on (registrations.bbl = oca.bbl);
+left join oca_evictions_bldgs as oca on (registrations.bbl = oca.bbl);
 
 drop table if exists wow_bldgs cascade;
 alter table wow_bldgs_temporary rename to wow_bldgs;
