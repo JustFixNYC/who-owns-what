e0ad99ef627932418ca671cc066613ffc7987bb4
Maxwell Austensen <m.austensen@gmail.com>
2022-12-13 12:46:44 -0500

    make all column selections explicit (no *)

--- sql/search_function.sql
+++ sql/search_function.sql
@@ -6,8 +6,76 @@
 DROP FUNCTION IF EXISTS get_assoc_addrs_from_bbl(text);
 
 CREATE OR REPLACE FUNCTION get_assoc_addrs_from_bbl(_bbl text)
-RETURNS SETOF wow_bldgs AS $$
-  SELECT bldgs.* FROM wow_bldgs AS bldgs
+RETURNS TABLE (
+  housenumber text,
+  streetname text,
+  zip text,
+  boro text,
+  registrationid integer,
+  lastregistrationdate date,
+  registrationenddate date,
+  bbl char(10),
+  bin char(7),
+  corpnames text[],
+  businessaddrs text[],
+  ownernames json,
+  allcontacts jsonb,
+  totalviolations integer,
+  openviolations integer,
+  totalcomplaints integer,
+  recentcomplaints integer,
+  recentcomplaintsbytype json,
+  unitsres integer,
+  yearbuilt smallint,
+  lat double precision,
+  lng double precision,
+  evictions bigint,
+  rsunits2007 integer,
+  rsunitslatest integer,
+  rsunitslatestyear integer,
+  rsdiff integer,
+  yearstartedj51 smallint,
+  yearstarted421a smallint,
+  lastsaleacrisid text,
+  lastsaledate date,
+  lastsaleamount bigint,
+  evictionfilings integer
+) AS $$
+  SELECT 
+    bldgs.housenumber,
+    bldgs.streetname,
+    bldgs.zip,
+    bldgs.boro,
+    bldgs.registrationid,
+    bldgs.lastregistrationdate,
+    bldgs.registrationenddate,
+    bldgs.bbl,
+    bldgs.bin,
+    bldgs.corpnames,
+    bldgs.businessaddrs,
+    bldgs.ownernames,
+    bldgs.allcontacts,
+    bldgs.totalviolations,
+    bldgs.openviolations,
+    bldgs.totalcomplaints,
+    bldgs.recentcomplaints,
+    bldgs.recentcomplaintsbytype,
+    bldgs.unitsres,
+    bldgs.yearbuilt,
+    bldgs.lat,
+    bldgs.lng,
+    bldgs.evictions,
+    bldgs.rsunits2007,
+    bldgs.rsunitslatest,
+    bldgs.rsunitslatestyear,
+    bldgs.rsdiff,
+    bldgs.yearstartedj51,
+    bldgs.yearstarted421a,
+    bldgs.lastsaleacrisid,
+    bldgs.lastsaledate,
+    bldgs.lastsaleamount,
+    bldgs.evictionfilings
+  FROM wow_bldgs AS bldgs
   INNER JOIN (
     (SELECT DISTINCT registrationid FROM wow_bldgs r WHERE r.bbl = _bbl) userreg
     LEFT JOIN LATERAL
