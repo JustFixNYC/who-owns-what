8a5f82d0293844b69801e0799d78eaaad6a121d6
Maxwell Austensen <m.austensen@gmail.com>
2023-01-20 00:03:31 -0500

    change inline SVG to address new errors

---
+++ client/src/assets/img/CloseIcon.tsx
@@ -0,0 +1,10 @@
+import React, { SVGProps} from 'react';
+
+const CloseIcon = (props: SVGProps<SVGSVGElement>) => (
+    <svg fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M2 2L14 14" fill="currentcolor" stroke-linecap="square"/>
+    <path d="M14 2L2 14" fill="currentcolor" stroke-linecap="square"/>
+    </svg>
+)
+
+export default CloseIcon
