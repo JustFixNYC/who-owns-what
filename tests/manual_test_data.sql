8de95e871260ee6bd43fe94d4ad7422809a06c6c
Maxwell Austensen <maxwell@justfix.nyc>
2023-01-30 11:56:18 -0500

    Add 2021 rent stab data (#702)
    Adds newly 2021 rent stabilized units data from nyc-doffer. No front end changes, just updates `rsunitslatest` in the wow_bldgs table from the updated `rentstab_v2` table in nycdb.

---
+++ tests/manual_test_data.sql
@@ -0,0 +1,9 @@
+--
+-- Manual creation of sample wow_portfolios table for testing:
+--
+
+INSERT INTO public.wow_portfolios (bbls, landlord_names, graph) VALUES ('{3016780054}','{"DAVID SIMKOWITZ"}','{"nodes": [{"id": 1, "value": {"kind": "bizaddr", "value": "206 JOHNSON AVENUE, STATEN ISLAND NY"}}, {"id": 2, "value": {"kind": "name", "value": "ANTHONY DEMEO"}}], "edges": [{"from": 1, "to": 2, "reg_contacts": 1}]}');
+
+--
+-- Manual table creation complete
+--
