8e05f757a7b62184037e6e0702277a0c792fc5c6
Maxwell Austensen <maxwell@justfix.org>
2023-04-12 17:51:49 -0400

    Minmax select refactor and add presets (#749)
    A big set of changes to our building size filter.
    
    The data structure of the options/selections ranges is changed from [number, number] to a more sensible [{min: number, max: number}], (an array because we can no have multiple ranges - see below). Also instead of using undefined as the default value, it uses -Infinity/Infinity (the undefined were changed to this anyway in the table filter application)
    The error handling for the min/max inputs also uses a similar data structure (plus a msg field) for better readability
    There are now a set of preset unit ranges to choose from (can be multiple), or you can expand a dropdown to reveal the original min/max inputs to define a custom range. When you enter anything in the custom inputs it disables the presets (but remembers your selections if you then clear the inputs). There's still the one apply button for both options.
    There are also a lot of a11y fixes in here: input options following these suggestions, proper labels and descriptions for screen readers (including the new presets), focus states, alert message.

--- client/src/components/MinMaxSelect.tsx
+++ client/src/components/MinMaxSelect.tsx
@@ -1,89 +1,203 @@
-import React from "react";
-import { Trans } from "@lingui/macro";
+import React, { InputHTMLAttributes } from "react";
+import { Trans, t } from "@lingui/macro";
+import { i18n } from "@lingui/core";
 import classnames from "classnames";
 import helpers from "util/helpers";
 import { Alert } from "./Alert";
-import { FilterNumberRange, MINMAX_DEFAULT } from "./PropertiesList";
+import { FilterNumberRange, NUMBER_RANGE_DEFAULT } from "./PropertiesList";
+
+type CustomRangeErrors = { min: boolean; max: boolean; msg: JSX.Element | undefined };
+const CUSTOM_RANGE_ERRORS_DEFAULT = { min: false, max: false, msg: undefined };
+
+type Preset = FilterNumberRange & { checked: boolean };
+const PRESETS_DEFAULT = [
+  { min: 1, max: 5, checked: false },
+  { min: 6, max: 29, checked: false },
+  { min: 30, max: 59, checked: false },
+  { min: 60, max: 99, checked: false },
+  { min: 100, max: 199, checked: false },
+  { min: 200, max: Infinity, checked: false },
+];
 
 function MinMaxSelect(props: {
   options: FilterNumberRange;
-  onApply: (selectedList: FilterNumberRange) => void;
+  onApply: (selections: FilterNumberRange[]) => void;
   id?: string;
   onFocusInput?: () => void;
 }) {
   const { options, onApply, id, onFocusInput } = props;
-  const [minMax, setMinMax] = React.useState<FilterNumberRange>(MINMAX_DEFAULT);
-  const [minMaxErrors, setMinMaxErrors] = React.useState<MinMaxErrors>([false, false, undefined]);
+  const [customRange, setCustomRange] = React.useState<FilterNumberRange>(NUMBER_RANGE_DEFAULT);
+  const [customRangeErrors, setCustomRangeErrors] = React.useState<CustomRangeErrors>(
+    CUSTOM_RANGE_ERRORS_DEFAULT
+  );
+  const [presets, setPresets] = React.useState<Preset[]>(PRESETS_DEFAULT);
+
+  const hasCustomInputs = isFinite(customRange.min) || isFinite(customRange.max);
+
+  const handlePresetChange = (preset: Preset, i: number) => {
+    setPresets((prev) => {
+      prev.splice(i, 1, { ...preset, checked: !preset.checked });
+      return [...prev];
+    });
+  };
+
+  const handleApply = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    if (isFinite(customRange.min) || isFinite(customRange.max)) {
+      const errors = minMaxHasError(customRange, options);
+      if (errors.min || errors.max) {
+        setCustomRangeErrors(errors);
+        return;
+      }
+      onApply([customRange]);
+    } else {
+      const presetSelections = presets
+        .filter((preset) => preset.checked)
+        .map((preset) => {
+          const { checked, ...range } = preset;
+          return range;
+        });
+      const selections = presetSelections.length ? presetSelections : [NUMBER_RANGE_DEFAULT];
+      onApply(selections);
+    }
+  };
+
+  const commonInputProps: InputHTMLAttributes<HTMLInputElement> = {
+    type: "text",
+    inputMode: "numeric",
+    pattern: "[0-9]*",
+    "aria-autocomplete": "none",
+    autoComplete: "off",
+    "aria-describedby": `${id || "minmax-select"}__custom-a11y-text`,
+    min: options.min,
+    max: options.min,
+    onKeyDown: helpers.preventNonNumericalInput,
+    onFocus: onFocusInput,
+  };
 
   return (
-    <form id={id} className="minmax-container">
-      {minMaxErrors[0] || minMaxErrors[1] ? (
-        <div className="alerts-container">
-          <Alert type="error" variant="primary" closeType="none" role="status">
-            {minMaxErrors[2]}
-          </Alert>
-        </div>
-      ) : (
-        <></>
-      )}
-      <div className="label-input-container">
-        <div className="labels-container">
-          <label htmlFor="min-input">
-            <Trans>MIN</Trans>
-          </label>
-          <label htmlFor="max-input">
-            <Trans>MAX</Trans>
-          </label>
-        </div>
-        <div className="inputs-container">
-          <input
-            id={`${id || "minmax-select"}_min-input`}
-            type="number"
-            min={options[0]}
-            max={options[1]}
-            value={minMax[0] == null ? "" : minMax[0]}
-            onKeyDown={helpers.preventNonNumericalInput}
-            onChange={(e) => {
-              setMinMaxErrors([false, false, undefined]);
-              setMinMax([cleanNumberInput(e.target.value), minMax[1]]);
-            }}
-            onFocus={onFocusInput}
-            className={classnames("min-input", { hasError: minMaxErrors[0] })}
-          />
-          <span>
-            <Trans>and</Trans>
-          </span>
-          <input
-            id={`${id || "minmax-select"}_max-input`}
-            type="number"
-            min={options[0]}
-            max={options[1]}
-            value={minMax[1] == null ? "" : minMax[1]}
-            onKeyDown={helpers.preventNonNumericalInput}
-            onChange={(e) => {
-              setMinMaxErrors([false, false, undefined]);
-              setMinMax([minMax[0], cleanNumberInput(e.target.value)]);
-            }}
-            onFocus={onFocusInput}
-            className={classnames("max-input", { hasError: minMaxErrors[1] })}
-          />
-        </div>
-      </div>
+    <div id={id} className="minmaxselect-container">
+      <fieldset
+        className="minmaxselect__presets-container"
+        aria-labelledby={`${id || "minmax-select"}__preset-a11y-text`}
+        tabIndex={hasCustomInputs ? 0 : -1}
+      >
+        {presets.map((preset, i) => (
+          <div className="minmaxselect__preset-value" key={i}>
+            <input
+              type="checkbox"
+              id={`minmaxselect__preset-${i}`}
+              name={`minmaxselect__preset-${i}`}
+              className={classnames({ checked: preset.checked })}
+              aria-describedby={`${id || "minmax-select"}__preset-a11y-text`}
+              onChange={() => handlePresetChange(preset, i)}
+              disabled={hasCustomInputs}
+              aria-hidden={hasCustomInputs}
+              checked={preset.checked}
+            />
+            <label htmlFor={`minmaxselect__preset-${i}`}>
+              {preset.min}
+              {isFinite(preset.max) ? `-${preset.max}` : "+"}
+            </label>
+          </div>
+        ))}
+        <span
+          id={`${id || "minmax-select"}__preset-a11y-text`}
+          className="minmaxselect__a11y-text"
+          hidden
+        >
+          {hasCustomInputs ? (
+            <Trans>Clear custom range inputs to use preset ranges.</Trans>
+          ) : (
+            <Trans>
+              Select one or more ranges of building units, or set a custom range, then apply
+              selections to filter the list of portfolio properties.
+            </Trans>
+          )}
+        </span>
+      </fieldset>
+      <details>
+        <summary className="minmaxselect__custom-range-summary">
+          <Trans>Custom Range</Trans>
+        </summary>
+        <form id={`${id || "minmaxselect"}__form`} className="minmaxselect__custom-range-container">
+          {customRangeErrors.min || customRangeErrors.max ? (
+            <div className="minmaxselect__alerts-container">
+              <Alert type="error" variant="primary" closeType="none" role="alert">
+                {customRangeErrors.msg}
+              </Alert>
+            </div>
+          ) : (
+            <></>
+          )}
+          <div className="minmaxselect__label-input-container">
+            <div className="minmaxselect__labels-container">
+              <label htmlFor={`${id || "minmax-select"}_min-input`}>
+                <Trans>MIN</Trans>
+              </label>
+              <label htmlFor={`${id || "minmax-select"}_max-input`}>
+                <Trans>MAX</Trans>
+              </label>
+            </div>
+            <div className="minmaxselect__inputs-container">
+              <input
+                id={`${id || "minmax-select"}_min-input`}
+                className={classnames("minmaxselect__min-input", {
+                  hasError: customRangeErrors.min,
+                })}
+                value={isFinite(customRange.min) ? customRange.min : ""}
+                onChange={(e) => {
+                  setCustomRangeErrors(CUSTOM_RANGE_ERRORS_DEFAULT);
+                  setCustomRange({
+                    min: cleanNumberInput(e.target.value) || -Infinity,
+                    max: customRange.max,
+                  });
+                }}
+                {...commonInputProps}
+              />
+              <span>
+                <Trans>and</Trans>
+              </span>
+              <input
+                id={`${id || "minmax-select"}_max-input`}
+                className={classnames("minmaxselect__max-input", {
+                  hasError: customRangeErrors.max,
+                })}
+                value={isFinite(customRange.max) ? customRange.max : ""}
+                onChange={(e) => {
+                  setCustomRangeErrors(CUSTOM_RANGE_ERRORS_DEFAULT);
+                  setCustomRange({
+                    min: customRange.min,
+                    max: cleanNumberInput(e.target.value) || Infinity,
+                  });
+                }}
+                {...commonInputProps}
+              />
+              <span
+                id={`${id || "minmax-select"}__custom-a11y-text`}
+                className="minmaxselect__a11y-text"
+                hidden
+              >
+                <Trans>
+                  Enter minimum and maximum number of units, or leave either blank, then apply
+                  selections to filter the list of portfolio properties. Set both to blank and apply
+                  to clear filter.
+                </Trans>
+              </span>
+            </div>
+          </div>
+        </form>
+      </details>
       <button
-        onClick={(e) => {
-          e.preventDefault();
-          const errors = minMaxHasError(minMax, options);
-          if (errors[0] || errors[1]) {
-            setMinMaxErrors(errors);
-          } else {
-            onApply(minMax);
-          }
-        }}
+        type="submit"
+        form={`${id || "minmaxselect"}__form`}
         className="button is-primary"
+        aria-label={i18n._(t`Apply selections and get results`)}
+        onClick={handleApply}
       >
         <Trans>Apply</Trans>
       </button>
-    </form>
+    </div>
   );
 }
 
@@ -92,37 +206,36 @@ function cleanNumberInput(value: string): number | undefined {
   return Number(value);
 }
 
-type MinMaxErrors = [boolean, boolean, JSX.Element | undefined];
-function minMaxHasError(values: FilterNumberRange, options: FilterNumberRange): MinMaxErrors {
-  const [minValue, maxValue] = values;
-  const [minOption, maxOption] = options;
+function minMaxHasError(values: FilterNumberRange, options: FilterNumberRange): CustomRangeErrors {
+  const { min: minValue, max: maxValue } = values;
+  const { min: minOption, max: maxOption } = options;
   let minHasError = false;
   let maxHasError = false;
   let errorMessage: JSX.Element | undefined;
 
-  if (minValue != null && maxValue != null && minValue > maxValue) {
+  if (isFinite(minValue) && isFinite(maxValue) && minValue > maxValue) {
     minHasError = true;
     maxHasError = true;
     errorMessage = <Trans>Min must be less than or equal to Max</Trans>;
   }
-  if (minValue != null && minValue < 0) {
+  if (isFinite(minValue) && minValue < 0) {
     minHasError = true;
     errorMessage = <Trans>Min must be greater than 0</Trans>;
   }
-  if (maxValue != null && maxValue < 0) {
+  if (isFinite(maxValue) && maxValue < 0) {
     minHasError = true;
     errorMessage = <Trans>Max must be greater than 0</Trans>;
   }
-  if (maxValue != null && minOption != null && maxValue < minOption) {
+  if (isFinite(maxValue) && isFinite(minOption) && maxValue < minOption) {
     maxHasError = true;
     errorMessage = <Trans>Max must be greater than {minOption}</Trans>;
   }
-  if (minValue != null && maxOption != null && minValue > maxOption) {
+  if (isFinite(minValue) && isFinite(maxOption) && minValue > maxOption) {
     minHasError = true;
     errorMessage = <Trans>Min must be less than {maxOption}</Trans>;
   }
 
-  return [minHasError, maxHasError, errorMessage];
+  return { min: minHasError, max: maxHasError, msg: errorMessage };
 }
 
 export default MinMaxSelect;
--- client/src/components/PortfolioTable.tsx
+++ client/src/components/PortfolioTable.tsx
@@ -28,7 +28,12 @@ import { SupportedLocale } from "../i18n-base";
 import Helpers, { longDateOptions } from "../util/helpers";
 import { logAmplitudeEvent } from "./Amplitude";
 import { AddressRecord, HpdComplaintCount } from "./APIDataTypes";
-import { FilterContext, IFilterContext, MINMAX_DEFAULT } from "./PropertiesList";
+import {
+  FilterContext,
+  FilterNumberRange,
+  IFilterContext,
+  NUMBER_RANGE_DEFAULT,
+} from "./PropertiesList";
 import "styles/PortfolioTable.scss";
 import { sortContactsByImportance } from "./DetailView";
 import { ArrowIcon } from "./Icons";
@@ -42,7 +47,7 @@ export const MAX_TABLE_ROWS_PER_PAGE = 100;
 declare module "@tanstack/table-core" {
   interface FilterFns {
     arrIncludesSome: FilterFn<unknown>;
-    inNumberRange: FilterFn<unknown>;
+    inNumberRanges: FilterFn<unknown>;
     isNonZero: FilterFn<unknown>;
   }
 }
@@ -56,6 +61,19 @@ const currencyFormater = new Intl.NumberFormat("en-us", {
 
 const isNonZero: FilterFn<any> = (row, columnId, value, addMeta) =>
   value ? !!row.getValue(columnId) : true;
+
+const inNumberRanges: FilterFn<any> = (
+  row,
+  columnId,
+  filterValue: FilterNumberRange[],
+  addMeta
+) => {
+  const rowValue = row.getValue<number>(columnId);
+  return filterValue.reduce(
+    (acc, rng) => acc || (rowValue >= rng.min && rowValue <= rng.max),
+    false
+  );
+};
 
 type PortfolioTableProps = {
   data: AddressRecord[];
@@ -80,7 +98,8 @@ export const PortfolioTable = React.memo((props: PortfolioTableProps) => {
   const activeFilters = {
     rsunitslatestActive: filterSelections.rsunitslatest,
     ownernamesActive: !!filterSelections.ownernames.length,
-    unitsresActive: filterSelections.unitsres !== MINMAX_DEFAULT,
+    unitsresActive:
+      isFinite(filterSelections.unitsres[0].min) || isFinite(filterSelections.unitsres[0].max),
     zipActive: !!filterSelections.zip.length,
   };
 
@@ -194,7 +213,7 @@ export const PortfolioTable = React.memo((props: PortfolioTableProps) => {
             footer: (props) => props.column.id,
             enableColumnFilter: false,
             size: "auto",
-            filterFn: "inNumberRange",
+            filterFn: "inNumberRanges",
           },
         ],
       },
@@ -488,7 +507,7 @@ export const PortfolioTable = React.memo((props: PortfolioTableProps) => {
     columns,
     filterFns: {
       arrIncludesSome: filterFns.arrIncludesSome,
-      inNumberRange: filterFns.inNumberRange,
+      inNumberRanges: inNumberRanges,
       isNonZero: isNonZero,
     },
     state: {
@@ -711,8 +730,7 @@ function useFilterOptionsUpdater(
   table: Table<AddressRecord>
 ) {
   const ownernamesOptionValues = table.getColumn("ownernames").getFacetedUniqueValues();
-  const unitsresOptionValues =
-    table.getColumn("unitsres").getFacetedMinMaxValues() || MINMAX_DEFAULT;
+  const unitsresOptionValues = table.getColumn("unitsres").getFacetedMinMaxValues();
   const zipOptionValues = table.getColumn("zip").getFacetedUniqueValues();
 
   React.useEffect(() => {
@@ -723,7 +741,9 @@ function useFilterOptionsUpdater(
         ownernames: Array.from(new Set(Array.from(ownernamesOptionValues.keys()).flat())).sort(
           compareAlphaNumLast
         ),
-        unitsres: unitsresOptionValues,
+        unitsres: unitsresOptionValues
+          ? { min: unitsresOptionValues[0], max: unitsresOptionValues[1] }
+          : NUMBER_RANGE_DEFAULT,
         zip: Array.from(zipOptionValues.keys())
           .filter((zip) => zip != null)
           .sort(),
