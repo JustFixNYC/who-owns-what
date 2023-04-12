b454a6114e24debc408b36b8c1ee62029e8920a8
Maxwell Austensen <m.austensen@gmail.com>
2023-04-12 01:59:53 -0400

    add preselect range buttons

--- client/src/components/MinMaxSelect.tsx
+++ client/src/components/MinMaxSelect.tsx
@@ -9,6 +9,16 @@ import { FilterNumberRange, NUMBER_RANGE_DEFAULT } from "./PropertiesList";
 type MinMaxErrors = { min: boolean; max: boolean; msg: JSX.Element | undefined };
 const MIN_MAX_ERRORS_DEFAULT = { min: false, max: false, msg: undefined };
 
+type Preset = FilterNumberRange & { checked: boolean };
+const PRESETS_DEFAULT = [
+  { min: 1, max: 5, checked: false },
+  { min: 6, max: 29, checked: false },
+  { min: 30, max: 59, checked: false },
+  { min: 60, max: 99, checked: false },
+  { min: 100, max: 199, checked: false },
+  { min: 200, max: Infinity, checked: false },
+];
+
 function MinMaxSelect(props: {
   options: FilterNumberRange;
   onApply: (selections: FilterNumberRange[]) => void;
@@ -16,99 +26,144 @@ function MinMaxSelect(props: {
   onFocusInput?: () => void;
 }) {
   const { options, onApply, id, onFocusInput } = props;
-  const [minMax, setMinMax] = React.useState<FilterNumberRange[]>([NUMBER_RANGE_DEFAULT]);
+  const [minMax, setMinMax] = React.useState<FilterNumberRange>(NUMBER_RANGE_DEFAULT);
   const [minMaxErrors, setMinMaxErrors] = React.useState<MinMaxErrors>(MIN_MAX_ERRORS_DEFAULT);
+  const [presets, setPresets] = React.useState<Preset[]>(PRESETS_DEFAULT);
+
+  const handlePresetChange = (i: number) => {
+    const updatedPresets = presets;
+    updatedPresets[i].checked = !updatedPresets[i].checked;
+    setPresets(updatedPresets);
+  };
+
+  const handleApply = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    let selections: FilterNumberRange[];
+    if (isFinite(minMax.min) || isFinite(minMax.max)) {
+      const errors = minMaxHasError(minMax, options);
+      if (errors.min || errors.max) {
+        setMinMaxErrors(errors);
+        return;
+      }
+      selections = [minMax];
+    } else {
+      selections = presets
+        .filter((preset) => preset.checked)
+        .map((preset) => {
+          const { checked, ...range } = preset;
+          return range;
+        });
+    }
+    onApply(selections || [NUMBER_RANGE_DEFAULT]);
+  };
 
   return (
-    <form id={id} className="minmax-container">
-      {minMaxErrors.min || minMaxErrors.max ? (
-        <div className="alerts-container">
-          <Alert type="error" variant="primary" closeType="none" role="status">
-            {minMaxErrors.msg}
-          </Alert>
-        </div>
-      ) : (
-        <></>
-      )}
-      <div className="label-input-container">
-        <div className="labels-container">
-          <label htmlFor={`${id || "minmax-select"}_min-input`}>
-            <Trans>MIN</Trans>
-          </label>
-          <label htmlFor={`${id || "minmax-select"}_max-input`}>
-            <Trans>MAX</Trans>
-          </label>
-        </div>
-        <div className="inputs-container">
-          <input
-            id={`${id || "minmax-select"}_min-input`}
-            type="text"
-            inputMode="numeric"
-            pattern="[0-9]*"
-            aria-describedby={`${id || "minmax-select"}__a11y-text`}
-            min={options.min}
-            max={options.min}
-            value={isFinite(minMax[0].min) ? minMax[0].min : ""}
-            onKeyDown={helpers.preventNonNumericalInput}
-            onChange={(e) => {
-              setMinMaxErrors(MIN_MAX_ERRORS_DEFAULT);
-              setMinMax([
-                { min: cleanNumberInput(e.target.value) || -Infinity, max: minMax[0].max },
-              ]);
-            }}
-            onFocus={onFocusInput}
-            className={classnames("min-input", { hasError: minMaxErrors.min })}
-          />
-          <span>
-            <Trans>and</Trans>
-          </span>
-          <input
-            id={`${id || "minmax-select"}_max-input`}
-            type="text"
-            inputMode="numeric"
-            pattern="[0-9]*"
-            aria-describedby={`${id || "minmax-select"}__a11y-text`}
-            min={options.max}
-            max={options.max}
-            value={isFinite(minMax[0].max) ? minMax[0].max : ""}
-            onKeyDown={helpers.preventNonNumericalInput}
-            onChange={(e) => {
-              setMinMaxErrors(MIN_MAX_ERRORS_DEFAULT);
-              setMinMax([
-                { min: minMax[0].min, max: cleanNumberInput(e.target.value) || Infinity },
-              ]);
-            }}
-            onFocus={onFocusInput}
-            className={classnames("max-input", { hasError: minMaxErrors.max })}
-          />
-          <span
-            id={`${id || "minmax-select"}__a11y-text`}
-            className="minmaxselect__a11y-text"
-            hidden
-          >
-            <Trans>
-              Enter minimum and maximum number of units, or leave either blank. Set both to blank to
-              clear filter.
-            </Trans>
-          </span>
-        </div>
-      </div>
+    <div id={id} className="minmaxselect-container">
+      <fieldset className="minmaxselect__presets-container">
+        {PRESETS_DEFAULT.map((rng, i) => (
+          <div className="minmaxselect__preset-value" key={i}>
+            <input
+              type="checkbox"
+              id={`minmaxselect__preset-${i}`}
+              name={`minmaxselect__preset-${i}`}
+              onChange={() => handlePresetChange(i)}
+              disabled={isFinite(minMax.min) || isFinite(minMax.max)}
+            />
+            <label htmlFor={`minmaxselect__preset-${i}`}>
+              {rng.min}
+              {isFinite(rng.max) ? `-${rng.max}` : "+"}
+            </label>
+          </div>
+        ))}
+      </fieldset>
+      <details>
+        <summary className="minmaxselect__custom-range-summary">
+          <Trans>Custom Range</Trans>
+        </summary>
+        <form id={`${id || "minmaxselect"}__form`} className="minmaxselect__custom-range-container">
+          {minMaxErrors.min || minMaxErrors.max ? (
+            <div className="minmaxselect__alerts-container">
+              <Alert type="error" variant="primary" closeType="none" role="status">
+                {minMaxErrors.msg}
+              </Alert>
+            </div>
+          ) : (
+            <></>
+          )}
+          <div className="minmaxselect__label-input-container">
+            <div className="minmaxselect__labels-container">
+              <label htmlFor={`${id || "minmax-select"}_min-input`}>
+                <Trans>MIN</Trans>
+              </label>
+              <label htmlFor={`${id || "minmax-select"}_max-input`}>
+                <Trans>MAX</Trans>
+              </label>
+            </div>
+            <div className="minmaxselect__inputs-container">
+              <input
+                id={`${id || "minmax-select"}_min-input`}
+                className={classnames("minmaxselect__min-input", { hasError: minMaxErrors.min })}
+                type="text"
+                inputMode="numeric"
+                pattern="[0-9]*"
+                aria-describedby={`${id || "minmax-select"}__a11y-text`}
+                min={options.min}
+                max={options.min}
+                value={isFinite(minMax.min) ? minMax.min : ""}
+                onKeyDown={helpers.preventNonNumericalInput}
+                onChange={(e) => {
+                  setMinMaxErrors(MIN_MAX_ERRORS_DEFAULT);
+                  setMinMax({
+                    min: cleanNumberInput(e.target.value) || -Infinity,
+                    max: minMax.max,
+                  });
+                }}
+                onFocus={onFocusInput}
+              />
+              <span>
+                <Trans>and</Trans>
+              </span>
+              <input
+                id={`${id || "minmax-select"}_max-input`}
+                className={classnames("minmaxselect__max-input", { hasError: minMaxErrors.max })}
+                type="text"
+                inputMode="numeric"
+                pattern="[0-9]*"
+                aria-describedby={`${id || "minmax-select"}__a11y-text`}
+                min={options.max}
+                max={options.max}
+                value={isFinite(minMax.max) ? minMax.max : ""}
+                onKeyDown={helpers.preventNonNumericalInput}
+                onChange={(e) => {
+                  setMinMaxErrors(MIN_MAX_ERRORS_DEFAULT);
+                  setMinMax({ min: minMax.min, max: cleanNumberInput(e.target.value) || Infinity });
+                }}
+                onFocus={onFocusInput}
+              />
+              <span
+                id={`${id || "minmax-select"}__a11y-text`}
+                className="minmaxselect__a11y-text"
+                hidden
+              >
+                <Trans>
+                  Enter minimum and maximum number of units, or leave either blank. Set both to
+                  blank to clear filter.
+                </Trans>
+              </span>
+            </div>
+          </div>
+        </form>
+      </details>
       <button
+        type="submit"
+        form={`${id || "minmaxselect"}__form`}
         className="button is-primary"
         aria-label={i18n._(t`Apply selections and get results`)}
-        onClick={(e) => {
-          e.preventDefault();
-          const errors = minMaxHasError(minMax[0], options);
-          if (errors.min || errors.max) {
-            setMinMaxErrors(errors);
-          } else {
-            onApply(minMax);
-          }
-        }}
+        onClick={handleApply}
       >
         <Trans>Apply</Trans>
       </button>
-    </form>
+    </div>
   );
 }
 
