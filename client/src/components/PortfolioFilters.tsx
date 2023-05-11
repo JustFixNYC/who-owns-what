67a86b73fe8cd14249ff12b51600b4c0aabbfa8e
Maxwell Austensen <m.austensen@gmail.com>
2023-05-11 12:07:23 -0400

    test

--- client/src/components/PortfolioFilters.tsx
+++ client/src/components/PortfolioFilters.tsx
@@ -227,7 +227,8 @@ const PortfolioFiltersWithoutI18n = React.memo(
               onApply={onUnitsresApply}
               onError={() => logPortfolioAnalytics("filterError", { column: "unitsres" })}
               id="filter-unitsres-minmax"
-              // onFocusInput={handlePageShift}
+              // onFocusInput={() => helpers.scrollToBottom(".mobile-wrapper-dropdown")}
+              onFocusInput={handlePageShift}
               // onBlurInput={handlePageShift}
               isOpen={unitsresIsOpen}
               defaultSelections={unitsresSelections}
