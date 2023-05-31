93ab9fc3633fe5b32d4a191eadc23f3b7d639f41
Ki Wan Sim <kiwan.sim@gmail.com>
2023-05-31 18:11:16 +0000

    add all cors lol

--- project/settings.py
+++ project/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 }
 
 CORS_ALLOW_HEADERS = default_headers + ("Access-Control-Allow-Origin", "Set-Cookie")
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
