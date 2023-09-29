36fcaa711e98f29bb447a903ce123537a5a9a771
Maxwell Austensen <m.austensen@gmail.com>
2023-09-28 20:43:10 -0400

    dev deps in wrong place

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -12,7 +12,6 @@ module.exports = function override(config) {
     os: false, // require.resolve("os-browserify") can be polyfilled here if needed
     url: false, // require.resolve("url") can be polyfilled here if needed
     zlib: false, // require.resolve("browserify-zlib") can be polyfilled here if needed
-    'process/browser': require.resolve('process/browser'),
   });
   config.resolve.fallback = fallback;
   config.plugins = (config.plugins || []).concat([
