088991983ac4a66865387afd2c26d350fe084fed
Maxwell Austensen <m.austensen@gmail.com>
2023-12-01 11:46:50 -0500

    update portfolio graph tests to match nodes on bbl

--- client/src/components/PortfolioGraph.test.tsx
+++ client/src/components/PortfolioGraph.test.tsx
@@ -31,21 +31,13 @@ const sampleNodes: PortfolioGraphNode[] = [
 
 describe("getLandlordNodeIDFromAddress()", () => {
   it("correctly identifies a node ID", () => {
-    expect(getLandlordNodeIDFromAddress(sampleNodes, SAMPLE_ADDRESS_RECORDS[0])).toEqual(["1"]);
+    expect(
+      getLandlordNodeIDFromAddress(sampleNodes, { ...SAMPLE_ADDRESS_RECORDS[0], bbl: "1000010001" })
+    ).toEqual(["1"]);
   });
   it("returns an empty array if no matching nodes are found", () => {
-    expect(getLandlordNodeIDFromAddress(sampleNodes, SAMPLE_ADDRESS_RECORDS[1])).toEqual([]);
-  });
-  it("can find multiple node IDs if there are multiple matches ", () => {
     expect(
-      getLandlordNodeIDFromAddress(sampleNodes, {
-        ...SAMPLE_ADDRESS_RECORDS[0],
-        ownernames: [
-          { title: "CorporateOwner", value: "MOSES GUTMAN" },
-          { title: "IndividualOwner", value: "BOOP JONES" },
-          { title: "Agent", value: "IRRELEVANT TOM" },
-        ],
-      })
-    ).toEqual(["1", "2"]);
+      getLandlordNodeIDFromAddress(sampleNodes, { ...SAMPLE_ADDRESS_RECORDS[1], bbl: "5000010001" })
+    ).toEqual([]);
   });
 });
