2f1055ffca621bfbc8ba1ba900d7e3b837b16bc5
kiwansim <emily.kiwan.sim@gmail.com>
2023-12-08 11:03:04 -0500

    Feature: Rent Stabilized Units Indicator  (#838)
    * Add url params for timeline indicators (#830)
    
    * add indicator param to timeline route, and update history shallow on selection
    
    * fix optional route definition
    
    * replace url rather than adding to history
    
    * swap summary for timeline in route tests
    
    * fix links to timeline, reorg helper funs
    
    * reset url to default indicator if param is invalid or missing
    
    * remove console.log
    
    ---------
    
    Co-authored-by: kiwan <kiwan.sim@gmail.com>
    
    * Add Rent Stabilized Units indicator to Timeline (#837)
    
    * change rent stab latest year to 2022
    
    * add 2022 as rsunitslatestyear, update yml dependenc, add csv for tests
    
    * update nycdb src branc
    
    * add nycdb test data for rentstab and hpd_complaints
    
    * add rentstab indicator to timeline
    
    * update MonthlyTimelineData type to match SQL
    
    * remove stacked bar denoting market rate units, add horizontal line for latest total unit count
    
    * add link to THE CITY's map visualization
    
    * run prettier
    
    * fussing around with viz UI
    
    * change minor variable to trigger deploy
    
    * yaxis max handled in switch statement
    
    * run lingui extract
    
    ---------
    
    Co-authored-by: Maxwell Austensen <m.austensen@gmail.com>
    
    ---------
    
    Co-authored-by: Maxwell Austensen <maxwell@justfix.org>
    Co-authored-by: Maxwell Austensen <m.austensen@gmail.com>

--- sql/create_bldgs_table.sql
+++ sql/create_bldgs_table.sql
@@ -29,8 +29,8 @@ rentstab as (
   select
     ucbbl,
     coalesce(unitsstab2007, 0) rsunits2007,
-    coalesce(uc2021, 0) rsunitslatest,
-    coalesce(uc2021, 0) - coalesce(unitsstab2007, 0) rsdiff
+    coalesce(uc2022, 0) rsunitslatest,
+    coalesce(uc2022, 0) - coalesce(unitsstab2007, 0) rsdiff
   from rentstab_summary
   left join rentstab_v2 using(ucbbl)
 ),
@@ -102,7 +102,7 @@ select distinct on (registrations.bbl)
   rentstab.rsunits2007,
   rentstab.rsunitslatest,
   -- Year of most recent rent stab data:
-  2021 as rsunitslatestyear,
+  2022 as rsunitslatestyear,
   rentstab.rsdiff,
   exemptions.yearstartedj51::smallint,
   exemptions.yearstarted421a::smallint,
