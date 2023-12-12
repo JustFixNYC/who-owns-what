04a34189a8462be7dfca560fddfb4b90d708904b
Maxwell Austensen <m.austensen@gmail.com>
2023-12-12 11:13:59 -0500

    update graph viz tests

--- client/src/components/PortfolioGraph.test.tsx
+++ client/src/components/PortfolioGraph.test.tsx
@@ -4,28 +4,25 @@ import { SAMPLE_ADDRESS_RECORDS } from "../state-machine-sample-data";
 
 const sampleNodes: PortfolioGraphNode[] = [
   {
-    id: 1,
-    value: {
-      name: "MOSES GUTMAN",
-      bizAddr: "101 MAIN ST QUEENS",
-      bbls: ["1000010001", "1000010002"],
-    },
+    id: "1",
+    type: "owner",
+    name: "MOSES GUTMAN",
+    bizAddr: "101 MAIN ST QUEENS",
+    bbls: ["1000010001", "1000010002"],
   },
   {
-    id: 2,
-    value: {
-      name: "MOSES GUTMAN",
-      bizAddr: "101 MAIN ST 2FL QUEENS",
-      bbls: ["1000010003"],
-    },
+    id: "2",
+    type: "owner",
+    name: "MOSES GUTMAN",
+    bizAddr: "101 MAIN ST 2FL QUEENS",
+    bbls: ["1000010003"],
   },
   {
-    id: 3,
-    value: {
-      name: "MOSES GUTTMAN",
-      bizAddr: "101 MAIN ST 2FL QUEENS",
-      bbls: ["1000010004", "1000010005", "1000010006"],
-    },
+    id: "3",
+    type: "owner",
+    name: "MOSES GUTTMAN",
+    bizAddr: "101 MAIN ST 2FL QUEENS",
+    bbls: ["1000010004", "1000010005", "1000010006"],
   },
 ];
 
