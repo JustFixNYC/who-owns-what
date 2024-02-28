ab785f529dd48165e801f3ee07464fa54486cee0
kiwan <kiwan.sim@gmail.com>
2024-02-27 19:20:18 -0500

    remove landing page for verify email button

--- client/src/containers/VerifyEmailPage.tsx
+++ client/src/containers/VerifyEmailPage.tsx
@@ -1,16 +1,16 @@
 import { useState, useEffect } from "react";
-import Page from "../components/Page";
 import { withI18n, withI18nProps } from "@lingui/react";
 import { Trans, t } from "@lingui/macro";
 import AuthClient, { VerifyStatusCode } from "../components/AuthClient";
 import { JustfixUser } from "state-machine";
+import Page from "components/Page";
 
 const VerifyEmailPage = withI18n()((props: withI18nProps) => {
   const { i18n } = props;
   const [user, setUser] = useState<JustfixUser>();
   const [isVerified, setIsVerified] = useState(false);
-  const [isLinkExpired, setIsLinkExpired] = useState(false);
-  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [isExpired, setIsExpired] = useState(false);
+  const [isEmailResent, setIsEmailResent] = useState(false);
 
   useEffect(() => {
     const asyncFetchUser = async () => {
@@ -29,19 +29,37 @@ const VerifyEmailPage = withI18n()((props: withI18nProps) => {
     asyncFetchUser();
   }, []);
 
-  const asyncVerifyEmail = async () => {
-    return await AuthClient.verifyEmail();
-  };
+  useEffect(() => {
+    const asyncVerifyEmail = async () => {
+      return await AuthClient.verifyEmail();
+    };
 
-  const renderLandingPage = () => {
-    const expiredLinkPage = (
+    asyncVerifyEmail().then((result) => {
+      if (
+        result.statusCode === VerifyStatusCode.Success ||
+        result.statusCode === VerifyStatusCode.AlreadyVerified
+      ) {
+        setIsVerified(true);
+      }
+
+      if (
+        result.statusCode === VerifyStatusCode.Expired ||
+        result.statusCode === VerifyStatusCode.Unknown
+      ) {
+        setIsExpired(true);
+      }
+    });
+  }, []);
+
+  const expiredLinkPage = () => {
+    const resendEmailPage = (
       <div className="text-center">
         <Trans render="h4">The link sent to you has timed out.</Trans>
         <br />
         <button
           className="button is-secondary"
           onClick={async () => {
-            setShowConfirmation(await AuthClient.resendVerifyEmail());
+            setIsEmailResent(await AuthClient.resendVerifyEmail());
           }}
         >
           <Trans>Resend verification email</Trans>
@@ -49,42 +67,8 @@ const VerifyEmailPage = withI18n()((props: withI18nProps) => {
       </div>
     );
 
-    const validLinkPage = (
+    const resendEmailConfirmation = (
       <div className="text-center">
-        <Trans render="h4" className="text-left">
-          {" "}
-          Verify this email:{" "}
-        </Trans>
-        <br />
-        <h4>{!!user && user.email}</h4>
-        <br />
-        <Trans render="h4" className="text-left">
-          {" "}
-          to receive Data Updates from Who Owns What.{" "}
-        </Trans>
-        <button
-          className="button is-primary"
-          onClick={() => {
-            asyncVerifyEmail().then((result) => {
-              const isVerified =
-                result.statusCode === VerifyStatusCode.Success ||
-                result.statusCode === VerifyStatusCode.AlreadyVerified;
-              setIsVerified(isVerified);
-              setIsLinkExpired(result.statusCode === VerifyStatusCode.Expired);
-              setShowConfirmation(isVerified);
-            });
-          }}
-        >
-          <Trans>Verify email</Trans>
-        </button>
-      </div>
-    );
-    return isLinkExpired ? expiredLinkPage : validLinkPage;
-  };
-
-  const renderConfirmationPage = () => {
-    const resendEmailPage = (
-      <>
         <Trans render="h4">Check your email inbox & spam</Trans>
         <br />
         <Trans>
@@ -92,25 +76,25 @@ const VerifyEmailPage = withI18n()((props: withI18nProps) => {
           few minutes to arrive. Once your email has been verified, you’ll be signed up for Data
           Updates.
         </Trans>
-      </>
+      </div>
     );
 
-    const successPage = () => (
-      <>
-        <Trans render="h4">Email verified</Trans>
-        <br />
-        <Trans render="h4">You are now signed up for weekly Data Updates.</Trans>
-      </>
-    );
-
-    return isVerified ? successPage() : resendEmailPage;
+    return isEmailResent ? resendEmailConfirmation : resendEmailPage;
   };
+
+  const successPage = () => (
+    <div className="text-center">
+      <Trans render="h4">Email verified</Trans>
+      <br />
+      <Trans render="h4">You are now signed up for weekly Data Updates.</Trans>
+    </div>
+  );
 
   return (
     <Page title={i18n._(t`Verify your email address`)}>
       <div className="TextPage Page">
         <div className="page-container">
-          {showConfirmation ? renderConfirmationPage() : renderLandingPage()}
+          {isVerified ? successPage() : isExpired && expiredLinkPage()}
         </div>
       </div>
     </Page>
