8ca4cde927b7c29589a7fbb560dfad96a0b91fb1
Maxwell Austensen <m.austensen@gmail.com>
2024-04-01 16:53:55 -0400

    remove "/overview" from hpd_link on api, fix substr typo

--- wow/sql/alerts_hpd_link.sql
+++ wow/sql/alerts_hpd_link.sql
@@ -1,11 +1,11 @@
 SELECT
   CASE 
     WHEN hpdbuildingid is not null AND hpdbuildings = 1
-    	THEN concat('https://hpdonline.nyc.gov/hpdonline/building/', hpdbuildingid, '/overview')
+    	THEN concat('https://hpdonline.nyc.gov/hpdonline/building/', hpdbuildingid)
     ELSE
         concat(
           'https://hpdonline.nyc.gov/hpdonline/building/search-results', 
-          '?boroId=', strsub(bbl, 1, 1),
+          '?boroId=', substr(bbl, 1, 1),
         	'&block=', substr(bbl, 2, 5), 
           '&lot=', substr(bbl, 7, 4)
         )
