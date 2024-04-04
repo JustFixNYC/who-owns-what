b5358cdbf48d55ee1710bbfa5771264862296e79
Maxwell Austensen <m.austensen@gmail.com>
2024-04-04 16:27:11 -0400

    fix desktop styles, add link to JF logo

--- client/src/components/StandalonePage.tsx
+++ client/src/components/StandalonePage.tsx
@@ -6,7 +6,7 @@ import { Link as JFCLLink } from "@justfixnyc/component-library";
 import "styles/StandalonePage.css";
 import Page from "./Page";
 import { createWhoOwnsWhatRoutePaths } from "routes";
-import { JFCLLocaleLink } from "i18n";
+import { JFCLLocaleLink, LocaleLink } from "i18n";
 import { Trans } from "@lingui/macro";
 import { JFLogo } from "./JFLogo";
 
@@ -15,7 +15,8 @@ export const StandalonePageFooter = () => {
   return (
     <Trans render="div" className="wow-footer">
       <JFCLLocaleLink to={home}>Who Owns What</JFCLLocaleLink> by JustFix.
-      <br/>Read our{" "}
+      <br />
+      Read our{" "}
       <JFCLLink
         href="https://www.justfix.org/en/privacy-policy/"
         target="_blank"
@@ -43,11 +44,14 @@ type StandalonePageProps = withI18nProps & {
 
 const StandalonePage = withI18n()((props: StandalonePageProps) => {
   const { title, className, children } = props;
+  const { home } = createWhoOwnsWhatRoutePaths();
   return (
     <Page title={title}>
       <div className={classNames("StandalonePage Page", className)}>
         <div className="page-container">
-          <JFLogo />
+          <LocaleLink to={home}>
+            <JFLogo className="jf-logo" />
+          </LocaleLink>
           <div className="standalone-container">{children}</div>
           <StandalonePageFooter />
         </div>
