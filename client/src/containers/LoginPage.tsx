6239118148979c5ef86103459161bb17cd4f9d35
kiwansim <emily.kiwan.sim@gmail.com>
2024-05-15 14:27:36 -0400

    Revert "add redirect login -> account when already logged in (#922)"
    This reverts commit 3b443ece2ce9aec02b8db0691b711c02c986622d.

--- client/src/containers/LoginPage.tsx
+++ client/src/containers/LoginPage.tsx
@@ -3,31 +3,11 @@ import { t } from "@lingui/macro";
 
 import StandalonePage from "components/StandalonePage";
 import Login from "components/Login";
-import { useContext, useEffect, useState } from "react";
-import { UserContext } from "components/UserContext";
-import { LocaleRedirect } from "i18n";
-import { createWhoOwnsWhatRoutePaths } from "routes";
-import { FixedLoadingLabel } from "components/Loader";
 
 const LoginPage = withI18n()((props: withI18nProps) => {
   const { i18n } = props;
-  const { account } = createWhoOwnsWhatRoutePaths();
-  const userContext = useContext(UserContext);
-  const fetchingUser = !userContext?.user;
-  const isLoggedIn = !!userContext?.user?.email;
-  const [redirectToLogin, setRedirectToLogin] = useState<boolean>();
 
-  useEffect(() => {
-    if (redirectToLogin === undefined && !fetchingUser) {
-      setRedirectToLogin(isLoggedIn);
-    }
-  }, [fetchingUser, redirectToLogin, isLoggedIn]);
-
-  return redirectToLogin === undefined ? (
-    <FixedLoadingLabel />
-  ) : redirectToLogin ? (
-    <LocaleRedirect to={{ pathname: account.settings }} />
-  ) : (
+  return (
     <StandalonePage title={i18n._(t`Log in / sign up`)} className="LoginPage">
       <Login />
     </StandalonePage>
