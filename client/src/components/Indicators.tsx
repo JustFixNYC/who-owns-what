2d3f618a3d7afde9a641f55a331481dd3221d61f
kiwansim <kiwan@justfix.org>
2024-09-26 07:34:50 -0700

    remove unused var

--- client/src/components/Indicators.tsx
+++ client/src/components/Indicators.tsx
@@ -230,7 +230,6 @@ class IndicatorsWithoutI18n extends Component<IndicatorsWithRouterProps, Indicat
       const { state, send } = this.props;
       const { detailAddr } = state.context.portfolioData;
       const {
-        defaultVis,
         activeVis,
         activeTimeSpan,
         monthsInGroup,
