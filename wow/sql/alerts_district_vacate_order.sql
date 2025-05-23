7e6379681f1aba08bb52f7e0e2567a75a9e64f59
Maxwell Austensen <maxwell@justfix.org>
2025-05-23 13:24:57 -0400

    add hpd_link to vacate orders api for area alerts (#1013)
    the hpd_link was missing from the vacate orders api for area alerts. it is already create in the indicators table so just adding it to the select
    
    also fixes the order or results

--- wow/sql/alerts_district_vacate_order.sql
+++ wow/sql/alerts_district_vacate_order.sql
@@ -8,19 +8,23 @@ SELECT
 	hpd_vacate_type,
 	hpd_vacate_units_affected,
 	hpd_vacate_reason,
+	hpd_link,
 	dob_vacate_date,
 	dob_vacate_type,
-	dob_vacate_complaint_number
+	dob_vacate_complaint_number,
+	COALESCE(dob_vacate_date, hpd_vacate_date) AS vacate_date
 FROM wow_indicators
 WHERE bbl IS NOT NULL
-	and (coun_dist = ANY(%(coun_dist)s)
+	and (
+		coun_dist = ANY(%(coun_dist)s)
 		or nta = ANY(%(nta)s)
 		or borough = ANY(%(borough)s)
 		or census_tract = ANY(%(census_tract)s)
-	    OR community_dist = ANY(%(community_dist)s::int[])
+	    or community_dist = ANY(%(community_dist)s::int[])
 		or cong_dist = ANY(%(cong_dist)s)
 		or assem_dist = ANY(%(assem_dist)s)
 		or stsen_dist = ANY(%(stsen_dist)s)
-		or zipcode = ANY(%(zipcode)s))
+		or zipcode = ANY(%(zipcode)s)
+	)
 	and (dob_vacate_date is not null
 	or hpd_vacate_date is not null);
