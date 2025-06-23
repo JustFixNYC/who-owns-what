66079a1692fa4c3a87bdf9116cf1eda6ac1994b7
Maxwell Austensen <m.austensen@gmail.com>
2025-06-23 13:19:45 -0400

    remove street view from summary page (desktop & mobile)

--- client/src/components/PropertiesSummary.tsx
+++ client/src/components/PropertiesSummary.tsx
@@ -13,7 +13,6 @@ import { StringifyListWithConjunction } from "./StringifyList";
 import { SocialShareAddressPage } from "./SocialShare";
 import { withMachineInStateProps } from "state-machine";
 import { ComplaintsSummary } from "./ComplaintsSummary";
-import { StreetViewStatic } from "./StreetView";
 
 type Props = withMachineInStateProps<"portfolioFound"> & {
   isVisible: boolean;
@@ -68,31 +67,6 @@ export default class PropertiesSummary extends Component<Props, {}> {
                   </Trans>
                 )}
               </p>
-              <aside>
-                {agg.violationsaddr.lat && agg.violationsaddr.lng && (
-                  <figure className="figure">
-                    <StreetViewStatic
-                      lat={agg.violationsaddr.lat}
-                      lng={agg.violationsaddr.lng}
-                      imgHeight={() => 800}
-                      imgWidth={() => 500}
-                      className="img-responsive"
-                    />
-                    <figcaption className="figure-caption text-center text-italic">
-                      <Trans>
-                        {agg.violationsaddr.housenumber} {agg.violationsaddr.streetname},{" "}
-                        {agg.violationsaddr.boro} currently has{" "}
-                        <Plural
-                          value={agg.violationsaddr.openviolations || 0}
-                          one="one open HPD violation"
-                          other="# open HPD violations"
-                        />{" "}
-                        - the most in this portfolio.
-                      </Trans>
-                    </figcaption>
-                  </figure>
-                )}
-              </aside>
               <Trans render="h6">Building info</Trans>
               <p>
                 <Trans>
@@ -129,42 +103,42 @@ export default class PropertiesSummary extends Component<Props, {}> {
               <EvictionsSummary {...agg} />
               <Trans render="h6">Rent stabilization</Trans>
               <RentstabSummary {...agg} />
-              <aside>
-                <div className="PropertiesSummary__links">
-                  <span className="PropertiesSummary__linksTitle">
-                    <Trans render="em">Additional links</Trans>
-                  </span>
-                  <div>
-                    <h6 className="PropertiesSummary__linksSubtitle">
-                      <Trans>Questions or feedback?</Trans>
-                    </h6>
-                    <Link
-                      href="mailto:support@justfix.org"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      icon="external"
-                    >
-                      support@justfix.org
-                    </Link>
-                  </div>
-                  <div>
-                    <h6 className="PropertiesSummary__linksSubtitle">
-                      <Trans>Share with your neighbors</Trans>
-                    </h6>
-                    <SocialShareAddressPage
-                      location="summary-tab"
-                      customContent={{
-                        tweet: t`This landlord owns ${agg.bldgs} buildings, and according to @NYCHousing, has received a total of ${agg.totalviolations} violations. Can you guess which landlord it is? Find their name and more data analysis here: `,
-                        tweetCloseout: t`#WhoOwnsWhat via @JustFixOrg`,
-                        emailSubject: t` This landlord’s buildings average ${agg.openviolationsperresunit} open HPD violations per apartment`,
-                        getEmailBody: (url: string) =>
-                          t`I was checking out this building on Who Owns What, a free landlord research tool from JustFix. It’s a remarkable website that every tenant and housing advocate should know about! Can you guess how many total violations this landlord portfolio has? Check it out here: ${url}`,
-                      }}
-                    />
-                  </div>
-                </div>
-              </aside>
             </div>
+            <aside>
+              <div className="PropertiesSummary__links">
+                <span className="PropertiesSummary__linksTitle">
+                  <Trans render="em">Additional links</Trans>
+                </span>
+                <div>
+                  <h6 className="PropertiesSummary__linksSubtitle">
+                    <Trans>Questions or feedback?</Trans>
+                  </h6>
+                  <Link
+                    href="mailto:support@justfix.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    icon="external"
+                  >
+                    support@justfix.org
+                  </Link>
+                </div>
+                <div>
+                  <h6 className="PropertiesSummary__linksSubtitle">
+                    <Trans>Share with your neighbors</Trans>
+                  </h6>
+                  <SocialShareAddressPage
+                    location="summary-tab"
+                    customContent={{
+                      tweet: t`This landlord owns ${agg.bldgs} buildings, and according to @NYCHousing, has received a total of ${agg.totalviolations} violations. Can you guess which landlord it is? Find their name and more data analysis here: `,
+                      tweetCloseout: t`#WhoOwnsWhat via @JustFixOrg`,
+                      emailSubject: t` This landlord’s buildings average ${agg.openviolationsperresunit} open HPD violations per apartment`,
+                      getEmailBody: (url: string) =>
+                        t`I was checking out this building on Who Owns What, a free landlord research tool from JustFix. It’s a remarkable website that every tenant and housing advocate should know about! Can you guess how many total violations this landlord portfolio has? Check it out here: ${url}`,
+                    }}
+                  />
+                </div>
+              </div>
+            </aside>
           </div>
           <LegalFooter />
         </div>
